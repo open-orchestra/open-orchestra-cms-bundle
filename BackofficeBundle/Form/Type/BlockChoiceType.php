d42b01704c2b96314cd308251c82c390528f42b9
itkg-ngilain <noel.gilain@businessdecision.com>
2014-09-16 10:10:27 +0200

    remove useless line

--- BackofficeBundle/Form/Type/BlockChoiceType.php
+++ BackofficeBundle/Form/Type/BlockChoiceType.php
@@ -3,7 +3,6 @@
 namespace PHPOrchestra\BackofficeBundle\Form\Type;
 
 use Symfony\Component\Translation\Translator;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
