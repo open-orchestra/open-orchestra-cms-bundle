d6914c64a5cda55e042999934f17fbcd03df3833
Nicolas Thal <nico.th4l@gmail.com>
2014-09-23 11:40:57 +0200

    add the login page

--- UserBundle/Document/User.php
+++ UserBundle/Document/User.php
@@ -6,9 +6,9 @@ use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
 
 /**
- * @MongoDB\Document(
- *   collection="user"
- * )
+ * Document User
+ *
+ * @MongoDB\Document(collection="user")
  */
 class User extends BaseUser
 {
--- UserBundle/PHPOrchestraUserBundle.php
+++ UserBundle/PHPOrchestraUserBundle.php
@@ -4,6 +4,16 @@ namespace PHPOrchestra\UserBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class PHPOrchestraUserBundle
+ */
 class PHPOrchestraUserBundle extends Bundle
 {
+    /**
+     * @return string
+     */
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
 }
