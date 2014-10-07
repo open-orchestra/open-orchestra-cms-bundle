12109ad332f622b6900852fdc4fa86ecbca63de9
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-10-07 14:59:03 +0200

    add test and skipped it because bug when you create a node

--- BackofficeBundle/FunctionalTest/Controller/HomepageControllerTest.php
+++ BackofficeBundle/FunctionalTest/Controller/HomepageControllerTest.php
@@ -65,4 +65,38 @@ class HomepageControllerTest extends WebTestCase
         $this->assertEquals(1, $crawler->filter('a:contains("Communauté")')->count());
         $this->assertEquals(1, $crawler->filter('a:contains("Contact")')->count());
     }
+
+    /**
+     * test new Node
+     */
+    public function testNewNodePageHome()
+    {
+        $this->markTestSkipped();
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+
+        $form = $crawler->selectButton('Login')->form();
+        $form['_username'] = 'benjamin';
+        $form['_password'] = 'benjamin';
+
+        $crawler = $client->submit($form);
+        $crawler = $client->request('GET', '/admin/');
+        $crawler = $client->request('GET', '/api/context/site/2/www.bphpOrchestra.fr');
+        $crawler = $client->request('GET', '/admin/');
+
+        $nbLink = $crawler->filter('a')->count();
+
+        $crawler = $client->request('GET', '/admin/node/new/fixture_page_what_is_orchestra');
+
+        $formUser = $crawler->selectButton('node_submit')->form();
+
+        $formUser['node[name]'] = 'fixture test';
+        $formUser['node[alias]'] = 'page-test';
+
+        $crawler = $client->submit($formUser);
+        $crawler = $client->request('GET', '/admin/');
+
+        $this->assertEquals($nbLink+1, $crawler->filter('a')->count());
+    }
 }
