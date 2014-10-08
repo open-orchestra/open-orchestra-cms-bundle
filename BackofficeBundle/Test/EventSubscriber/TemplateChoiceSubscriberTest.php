6382b795c62c88b349fc718a3176aef4e2044643
Nicolas Thal <nico.th4l@gmail.com>
2014-10-08 15:53:48 +0200

    display the template choice only on new form

--- BackofficeBundle/Test/EventSubscriber/TemplateChoiceSubscriberTest.php
+++ BackofficeBundle/Test/EventSubscriber/TemplateChoiceSubscriberTest.php
@@ -53,6 +53,7 @@ class TemplateChoiceSubscriberTest extends \PHPUnit_Framework_TestCase
     public function testEventSubscribed()
     {
         $this->assertArrayHasKey(FormEvents::PRE_SUBMIT, $this->subscriber->getSubscribedEvents());
+        $this->assertArrayHasKey(FormEvents::PRE_SET_DATA, $this->subscriber->getSubscribedEvents());
     }
 
     /**
@@ -118,5 +119,74 @@ class TemplateChoiceSubscriberTest extends \PHPUnit_Framework_TestCase
 
         Phake::verify($templateChoiceContainer, Phake::never())->setAreas(Phake::anyParameters());
         Phake::verify($templateChoiceContainer, Phake::never())->setBlocks(Phake::anyParameters());
+    }
+
+    /**
+     * test the build form
+     *
+     * @param array $templates
+     * @param array $expectedResult
+     *
+     * @dataProvider getTemplate
+     */
+    public function testPreSetDataOnNewNode($templates, $expectedResult)
+    {
+        $node = Phake::mock('PHPOrchestra\ModelBundle\Model\NodeInterface');
+        Phake::when($this->event)->getData()->thenReturn($node);
+        Phake::when($this->templateRepository)->findByDeleted(Phake::anyParameters())->thenReturn($templates);
+
+        $this->subscriber->preSetData($this->event);
+
+        Phake::verify($this->form)->add('templateId', 'choice', array(
+            'choices' => $expectedResult,
+            'label' => 'php_orchestra_backoffice.form.node.template_id'
+        ));
+    }
+
+    /**
+     * Templates provider
+     *
+     * @return array
+     */
+    public function getTemplate()
+    {
+        $id0 = 'fakeId0';
+        $name0 = 'fakeName0';
+        $id1 = 'fakeId1';
+        $name1 = 'fakeName1';
+
+        $template0 = Phake::mock('PHPOrchestra\ModelBundle\Model\TemplateInterface');
+        Phake::when($template0)->getTemplateId()->thenReturn($id0);
+        Phake::when($template0)->getName()->thenReturn($name0);
+
+        $template1 = Phake::mock('PHPOrchestra\ModelBundle\Model\TemplateInterface');
+        Phake::when($template1)->getTemplateId()->thenReturn($id1);
+        Phake::when($template1)->getName()->thenReturn($name1);
+
+        return array(
+            array(
+                array($template0, $template1), array($id0 => $name0, $id1 => $name1)
+            )
+        );
+    }
+
+    /**
+     * test the build form
+     *
+     * @param array $templates
+     * @param array $expectedResult
+     *
+     * @dataProvider getTemplate
+     */
+    public function testPreSetDataWithExistingNode($templates, $expectedResult)
+    {
+        $node = Phake::mock('PHPOrchestra\ModelBundle\Model\NodeInterface');
+        Phake::when($node)->getId()->thenReturn('fakeId');
+        Phake::when($this->event)->getData()->thenReturn($node);
+        Phake::when($this->templateRepository)->findByDeleted(Phake::anyParameters())->thenReturn($templates);
+
+        $this->subscriber->preSetData($this->event);
+
+        Phake::verify($this->form, Phake::never())->add(Phake::anyParameters());
     }
 }
