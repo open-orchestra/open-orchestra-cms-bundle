6d266ec7b2609c3dcb6e9efa7e8aa3181d022896
itkg-ngilain <noel.gilain@businessdecision.com>
2014-10-10 12:25:17 +0200

    Dynamically calculate mediaModal dimensions & position

--- BackofficeBundle/Resources/public/js/orchestraMediaType.js
+++ BackofficeBundle/Resources/public/js/orchestraMediaType.js
@@ -3,8 +3,20 @@ $(document).on("click", ".mediaModalOpen", function(event) {
     var button = event.currentTarget;
     var modalId = $(button).data("target");
     var mediaModal = $("#" + modalId);
-    mediaModal.css("top", "-100px");
-    
+
+    var orchestraModal = $("#OrchestraBOModal");
+
+    var mediaModalDialog = mediaModal.find(".modal-dialog:first");
+    var left = (mediaModalDialog.width() - orchestraModal.width() - $.scrollbarWidth()) / 2;
+
+    var mediaModalBody = mediaModal.find(".modal-body:first");
+
+    mediaModal.css("top", "-88px");
+    mediaModal.css("left", left + "px");
+    mediaModal.css("width", orchestraModal.css('width'));
+    mediaModal.css("height", orchestraModal.css('height'));
+    mediaModalBody.css("min-height", (mediaModal.height() - 120) + "px");
+
     mediaModal.modal('show');
 });
 
@@ -14,5 +26,6 @@ $(document).on("click", ".mediaModalClose", function(event) {
     var modalId = $(button).data("target");
     var mediaModal = $("#" + modalId);
     mediaModal.modal('hide');
-    mediaModal.css("top", "250px");
+    mediaModal.css("width", "0px");
+    mediaModal.css("height", "0px");
 });
