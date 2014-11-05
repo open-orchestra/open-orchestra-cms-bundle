02acd0df71f7f338abe3fc91d06cc4370611ae09
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-11-05 15:25:13 +0100

    add a new block: content_list

--- BackofficeBundle/DependencyInjection/PHPOrchestraBackofficeExtension.php
+++ BackofficeBundle/DependencyInjection/PHPOrchestraBackofficeExtension.php
@@ -51,7 +51,8 @@ class PHPOrchestraBackofficeExtension extends Extension
             DisplayBlockInterface::TINYMCEWYSIWYG,
             DisplayBlockInterface::CONFIGURABLE_CONTENT,
             LoginStrategy::LOGIN,
-            DisplayBlockInterface::SUBMENU
+            DisplayBlockInterface::SUBMENU,
+            DisplayBlockInterface::CONTENTLIST
         );
 
         if (array_key_exists('blocks', $config) && !empty($config['blocks'])) {
---
+++ BackofficeBundle/DisplayBlock/Strategies/ContentListStrategy.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PHPOrchestra\BackofficeBundle\DisplayBlock\Strategies;
+
+use PHPOrchestra\DisplayBundle\DisplayBlock\DisplayBlockInterface;
+use PHPOrchestra\DisplayBundle\DisplayBlock\Strategies\AbstractStrategy;
+use PHPOrchestra\ModelBundle\Model\BlockInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ContentListStrategy
+ */
+class ContentListStrategy extends AbstractStrategy
+{
+    /**
+     * Check if the strategy support this block
+     *
+     * @param BlockInterface $block
+     *
+     * @return boolean
+     */
+    public function support(BlockInterface $block)
+    {
+        return DisplayBlockInterface::CONTENTLIST == $block->getComponent();
+    }
+
+    /**
+     * Perform the show action for a block
+     *
+     * @param BlockInterface $block
+     *
+     * @return Response
+     */
+    public function show(BlockInterface $block)
+    {
+        $attributes = $block->getAttributes();
+
+        return $this->render(
+            'PHPOrchestraBackofficeBundle:Block/ContentList:show.html.twig',
+            array(
+                'contentType' => $attributes['contentType']
+            )
+        );
+    }
+
+    /**
+     * Get the name of the strategy
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'content_list';
+    }
+}
