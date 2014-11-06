6a9f18b8d0d6257395f2294e2006ee86c9deced2
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-11-06 11:35:20 +0100

    add content block

---
+++ Backoffice/GenerateForm/Strategies/ContentStrategy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PHPOrchestra\Backoffice\GenerateForm\Strategies;
+
+use PHPOrchestra\DisplayBundle\DisplayBlock\DisplayBlockInterface;
+use PHPOrchestra\ModelBundle\Model\BlockInterface;
+use Symfony\Component\Form\FormInterface;
+
+/**
+ * Class ContentStrategy
+ */
+class ContentStrategy extends AbstractBlockStrategy
+{
+    /**
+     * @param BlockInterface $block
+     *
+     * @return bool
+     */
+    public function support(BlockInterface $block)
+    {
+        return DisplayBlockInterface::CONTENT === $block->getComponent();
+    }
+
+    /**
+     * @param FormInterface  $form
+     * @param BlockInterface $block
+     */
+    public function buildForm(FormInterface $form, BlockInterface $block)
+    {
+        $attributes = $block->getAttributes();
+
+        $form->add('class', 'textarea', array(
+            'mapped' => false,
+            'data' => array_key_exists('class', $attributes)? json_encode($attributes['class']):json_encode(
+                array(
+                    'div' => 'divclass',
+                    'title' => 'titleclass',
+                    'ul' => 'ulclass',
+                    'content' => 'contentclass'
+                    )
+            ),
+        ));
+        $form->add('id', 'text', array(
+            'mapped' => false,
+            'data' => array_key_exists('id', $attributes)? $attributes['id']:'',
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'content';
+    }
+}
---
+++ BackofficeBundle/DisplayBlock/Strategies/ContentStrategy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace PHPOrchestra\BackofficeBundle\DisplayBlock\Strategies;
+
+use PHPOrchestra\DisplayBundle\DisplayBlock\DisplayBlockInterface;
+use PHPOrchestra\DisplayBundle\DisplayBlock\Strategies\AbstractStrategy;
+use PHPOrchestra\ModelBundle\Model\BlockInterface;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class ContentStrategy
+ */
+class ContentStrategy extends AbstractStrategy
+{
+    /**
+     * Check if the strategy support this block
+     *
+     * @param BlockInterface $block
+     *
+     * @return boolean
+     */
+    public function support(BlockInterface $block)
+    {
+        return DisplayBlockInterface::CONTENT == $block->getComponent();
+    }
+
+    /**
+     * Perform the show action for a block
+     *
+     * @param BlockInterface $block
+     *
+     * @return Response
+     */
+    public function show(BlockInterface $block)
+    {
+        $attributes = $block->getAttributes();
+
+        return $this->render(
+            'PHPOrchestraBackofficeBundle:Block/Content:show.html.twig',
+            array(
+                'id' => $attributes['id'],
+                'class' => implode(' ', $attributes['class']),
+            )
+        );
+    }
+
+    /**
+     * Get the name of the strategy
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'content';
+    }
+}
