cb041da390f852a1531954f09531e518419ade24
itkg-bfouche <benjamin.fouche@businessdecision.com>
2014-11-24 10:56:40 +0100

    change array_key_exists by array_merge

--- BackofficeBundle/DisplayBlock/Strategies/ContentListStrategy.php
+++ BackofficeBundle/DisplayBlock/Strategies/ContentListStrategy.php
@@ -39,10 +39,10 @@ class ContentListStrategy extends AbstractStrategy
             'PHPOrchestraBackofficeBundle:Block/ContentList:show.html.twig',
             array(
                 'contentType' => $attributes['contentType'],
-                'id' => array_key_exists('id', $attributes)? $attributes['id'] : '',
-                'class' => array_key_exists('class', $attributes)? $attributes['class']: '',
+                'id' => $attributes['id'],
+                'class' => $attributes['class'],
                 'url' => $attributes['url'],
-                'characterNumber' => array_key_exists('characterNumber', $attributes)? $attributes['characterNumber']: '50',
+                'characterNumber' => $attributes['characterNumber'],
             )
         );
     }
