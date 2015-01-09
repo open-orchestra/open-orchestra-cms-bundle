fb2fd898dfd40fd31634708a99adff28e85794fc
itkg-bfouche <benjamin.fouche@businessdecision.com>
2015-01-09 15:09:14 +0100

    fix videoUrlToIdTransformer

--- BackofficeBundle/Form/DataTransformer/VideoUrlToIdTransformer.php
+++ BackofficeBundle/Form/DataTransformer/VideoUrlToIdTransformer.php
@@ -16,16 +16,7 @@ class VideoUrlToIdTransformer implements DataTransformerInterface
      */
     public function transform($url)
     {
-        $explode = explode("/", $url);
-        $videoId = $explode[0];
-
-        if (count($explode) > 1) {
-            $videoId = array_pop($explode);
-
-            if (count(explode('=', $videoId))) {
-                $videoId = array_pop(explode('=', $videoId));
-            }
-        }
+        $videoId = array_pop(preg_split('#/|=#', $url));
 
         return $videoId;
     }
