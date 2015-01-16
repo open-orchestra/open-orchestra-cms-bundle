2e60521765a5fa798c64049ad32d12b5bf630b83
itkg-ngilain <noel.gilain@businessdecision.com>
2015-01-16 11:00:10 +0100

    Basic organisation

--- BackofficeBundle/Command/OrchestraGenerateSitemapCommand.php
+++ BackofficeBundle/Command/OrchestraGenerateSitemapCommand.php
@@ -10,23 +10,53 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class OrchestraGenerateSitemapCommand extends ContainerAwareCommand
 {
+    /**
+     * Configure command
+     */
     protected function configure()
     {
         $this
             ->setName('orchestra:sitemap:generate')
             ->setDescription('Generate sitemaps')
-            ->addOption('siteId', null, InputOption::VALUE_OPTIONAL, 'If set, will generate sitemap only for this id')
-        ;
+            ->addOption('siteId', null, InputOption::VALUE_OPTIONAL, 'If set, will only generate sitemap for this site');
     }
 
+    /**
+     * Execute command
+     * 
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $text = "Génération de tous les sitemaps";
-    
-        if ($input->getOption('siteId')) {
-            $text = 'Generation uniquement pour le site ' . $input->getOption('siteId');
+        if ($siteId = $input->getOption('siteId')) {
+            $this->generateSitemap($siteId, $output);
+        } else {
+            $sites = array('1', '3', '6', '9');
+            foreach ($sites as $siteId) {
+                $this->generateSitemap($siteId, $output);
+            }
         }
 
-        $output->writeln($text);
+        $output->writeln("<info>Done.</info>");
+    }
+
+    /**
+     * Generate sitemap for siteId
+     * 
+     * @param string          $siteId
+     * @param OutputInterface $output
+     */
+    protected function generateSitemap($siteId, OutputInterface $output)
+    {
+        $output->writeln("<info>Generating sitemap for siteId " . $siteId . "</info>");
+
+        $filename = 'sitemap.' . $siteId . '.xml';
+        $text = 'random xml content';
+
+        file_put_contents('web/' . $filename, $text);
+        $output->writeln("<comment>-> " . $filename . " generated</comment>\n");
+
+        return true;
     }
 }
