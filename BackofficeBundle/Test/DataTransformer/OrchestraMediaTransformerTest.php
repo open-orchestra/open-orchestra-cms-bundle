0ffeeff52ef9a56a3763d5a16a110b1eca3170f1
Nicolas Thal <nico.th4l@gmail.com>
2015-01-16 18:24:00 +0100

    add media prefix

--- BackofficeBundle/Test/DataTransformer/OrchestraMediaTransformerTest.php
+++ BackofficeBundle/Test/DataTransformer/OrchestraMediaTransformerTest.php
@@ -2,11 +2,78 @@
 
 namespace PHPOrchestra\BackofficeBundle\Test\DataTransformer;
 
+use Phake;
+use PHPOrchestra\BackofficeBundle\Form\DataTransformer\OrchestraMediaTransformer;
 
 /**
- * Class OrchestraMediaTransformer
+ * Class OrchestraMediaTransformerTest
  */
-class OrchestraMediaTransformer
+class OrchestraMediaTransformerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var OrchestraMediaTransformer
+     */
+    protected $transformer;
 
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->transformer = new OrchestraMediaTransformer();
+    }
+
+    /**
+     * Test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\Form\DataTransformerInterface', $this->transformer);
+    }
+
+    /**
+     * @param string $value
+     * @param string $expected
+     *
+     * @dataProvider provideTransformData
+     */
+    public function testTransform($value, $expected)
+    {
+        $this->assertSame($expected, $this->transformer->transform($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function provideTransformData()
+    {
+        return array(
+            array('', ''),
+            array('media-id', 'id'),
+            array('id', 'id'),
+        );
+    }
+
+    /**
+     * @param string $value
+     * @param string $expected
+     *
+     * @dataProvider provideReverseTransformData
+     */
+    public function testReverseTransform($value, $expected)
+    {
+        $this->assertSame($expected, $this->transformer->reverseTransform($value));
+    }
+
+    /**
+     * @return array
+     */
+    public function provideReverseTransformData()
+    {
+        return array(
+            array('', ''),
+            array('id', 'media-id'),
+            array('media-id', 'media-id'),
+        );
+    }
 }
