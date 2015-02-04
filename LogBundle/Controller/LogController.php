7021f6453058260427fc46a52b39f59e73152301
itkg-bfouche <benjamin.fouche@businessdecision.com>
2015-02-04 13:47:19 +0100

    move logController into logBundle

--- LogBundle/Controller/LogController.php
+++ LogBundle/Controller/LogController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PHPOrchestra\ApiBundle\Controller;
+namespace PHPOrchestra\LogBundle\Controller;
 
 use PHPOrchestra\ApiBundle\Facade\FacadeInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
