5a34a149339cff927f9a6d153f871c204934d68a
Nicolas Thal <nico.th4l@gmail.com>
2015-04-24 15:49:44 +0200

    use abstract admin controller in user admin

--- UserAdminBundle/Controller/Admin/UserController.php
+++ UserAdminBundle/Controller/Admin/UserController.php
@@ -2,22 +2,20 @@
 
 namespace OpenOrchestra\UserAdminBundle\Controller\Admin;
 
+use OpenOrchestra\BackofficeBundle\Controller\AbstractAdminController;
 use OpenOrchestra\UserBundle\Event\UserEvent;
-use OpenOrchestra\UserBundle\Document\User;
 use OpenOrchestra\UserBundle\UserEvents;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
-use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Form\Form;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * Class UserController
  *
  * @Config\Route("user")
  */
-class UserController extends Controller
+class UserController extends AbstractAdminController
 {
     /**
      * @param Request $request
@@ -31,17 +29,22 @@ class UserController extends Controller
      */
     public function newAction(Request $request)
     {
-        $user = new User();
-        $url = $this->generateUrl('open_orchestra_user_admin_new');
-        $event = UserEvents::USER_CREATE;
-        $form = $this->handleForm($request, 'registration_user', $user, $url, $event);
-        if ($form->isValid()) {
+        $userClass = $this->container->getParameter('open_orchestra_user.document.user.class');
+        /** @var UserInterface $user */
+        $user = new $userClass();
+        $form = $this->createForm('registration_user', $user, array(
+            'action' => $this->generateUrl('open_orchestra_user_admin_new'),
+        ));
+        $form->handleRequest($request);
+        if ($this->handleForm($form, $this->get('translator')->trans('open_orchestra_user.new.success'), $user)) {
             $url = $this->generateUrl('open_orchestra_user_admin_user_form', array('userId' => $user->getId()));
+
+            $this->dispatchEvent(UserEvents::USER_CREATE, new UserEvent($user));
 
             return $this->redirect($url);
         }
 
-        return $this->renderForm($form);
+        return $this->renderAdminForm($form);
     }
 
     /**
@@ -58,72 +61,14 @@ class UserController extends Controller
     public function formAction(Request $request, $userId)
     {
         $user = $this->get('open_orchestra_user.repository.user')->find($userId);
-        $url = $this->generateUrl('open_orchestra_user_admin_user_form', array('userId' => $userId));
-        $event = UserEvents::USER_UPDATE;
-        $form = $this->handleForm($request, 'user', $user, $url, $event);
-
-        return $this->renderForm($form);
-    }
-
-    /**
-     * @param Request $request
-     * @param string  $formName
-     * @param User    $user
-     * @param string  $url
-     * @param string  $event
-     *
-     * @return Form
-     */
-    protected function handleForm(Request $request, $formName, User $user, $url, $event)
-    {
-        $form = $this->createForm($formName, $user, array(
-            'action' => $url
+        $form = $this->createForm('user', $user, array(
+            'action' => $this->generateUrl('open_orchestra_user_admin_user_form', array('userId' => $userId))
         ));
-
         $form->handleRequest($request);
-        if ($form->isValid()) {
-            $this->saveUser($user);
-            $this->dispatchEvent($event, new UserEvent($user, $request));
-        }
+        $this->handleForm($form, $this->get('translator')->trans('open_orchestra_user.update.success'));
 
-        return $form;
-    }
+        $this->dispatchEvent(UserEvents::USER_UPDATE, new UserEvent($user));
 
-    /**
-     * @param Form $form
-     *
-     * @return Response
-     */
-    protected function renderForm(Form $form)
-    {
-        return $this->render($this->container->getParameter('open_orchestra_user.form_template'), array(
-            'form' => $form->createView()
-        ));
-    }
-
-    /**
-     * @param $user
-     */
-    protected function saveUser(User $user)
-    {
-        $type = $user->getId() ? 'update' : 'new';
-
-        $documentManager = $this->get('doctrine.odm.mongodb.document_manager');
-        $documentManager->persist($user);
-        $documentManager->flush();
-
-        $this->get('session')->getFlashBag()->add(
-            'success',
-            $this->get('translator')->trans('open_orchestra_user.'.$type.'.success')
-        );
-    }
-
-    /**
-     * @param string $eventName
-     * @param Event  $event
-     */
-    protected function dispatchEvent($eventName, $event)
-    {
-        $this->get('event_dispatcher')->dispatch($eventName, $event);
+        return $this->renderAdminForm($form);
     }
 }
