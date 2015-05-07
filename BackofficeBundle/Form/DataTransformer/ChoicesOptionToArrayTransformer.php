ee02e812e5cb305eedf773e082913dd1a83796cd
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-05-07 17:59:26 +0200

    add content attribute choice

---
+++ BackofficeBundle/Form/DataTransformer/ChoicesOptionToArrayTransformer.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace OpenOrchestra\BackofficeBundle\Form\DataTransformer;
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * Class ChoicesOptionToArrayTransformer
+ */
+class ChoicesOptionToArrayTransformer implements DataTransformerInterface
+{
+    protected $suppressSpecialCharacterClass;
+
+    /**
+     * @param SuppressSpecialCharacterTransformer $suppressSpecialCharacterClass
+     */
+    public function __construct(SuppressSpecialCharacterTransformer $suppressSpecialCharacterClass)
+    {
+        $this->suppressSpecialCharacterClass = $suppressSpecialCharacterClass;
+    }
+
+    /**
+     * @param array $arrayChoices
+     * @return string
+     */
+    public function transform($arrayChoices)
+    {
+        if (null === $arrayChoices) {
+            return "";
+        }
+        return implode(',', $arrayChoices);
+    }
+
+    /**
+     * @param string $choices
+     * @return array choix1,choix2,choix3
+     */
+    public function reverseTransform($choices)
+    {
+        if (null === $choices || '' === trim($choices)) {
+            return array();
+        }
+
+        $choices = explode(',', $choices);
+        $arrayChoice = array();
+
+        foreach ($choices as $choice){
+            $choice = $this->suppressSpecialCharacterClass->transform($choice);
+            if( '' != $choice) {
+                $arrayChoice[$choice] = $choice;
+            }
+        }
+
+        return $arrayChoice;
+    }
+}
