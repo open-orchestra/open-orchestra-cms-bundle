feaad911f76d704ed112c48e61b04827628d1b7a
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-05-11 11:48:02 +0200

    load TwigGlobalsCompilerPass

--- MediaAdminBundle/DependencyInjection/OpenOrchestraMediaAdminExtension.php
+++ MediaAdminBundle/DependencyInjection/OpenOrchestraMediaAdminExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-
+use OpenOrchestra\MediaAdminBundle\DependencyInjection\Compiler\TwigGlobalsCompilerPass;
 /**
  * This is the class that loads and manages your bundle configuration
  *
@@ -30,5 +30,6 @@ class OpenOrchestraMediaAdminExtension extends Extension
         $loader->load('display.yml');
         $loader->load('icon.yml');
         $loader->load('extractreference.yml');
+        $container->addCompilerPass(new TwigGlobalsCompilerPass());
     }
 }
