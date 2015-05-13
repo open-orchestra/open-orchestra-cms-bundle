de183459827d0266f87e1d22fe874735734f1520
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-05-13 11:37:59 +0200

    rename workflow to workflowfonction

--- WorkflowFonctionAdminBundle/Transformer/WorkflowFonctionTransformer.php
+++ WorkflowFonctionAdminBundle/Transformer/WorkflowFonctionTransformer.php
@@ -30,11 +30,11 @@ class WorkflowFonctionTransformer extends AbstractTransformer
         ));
         $facade->addLink('_self_delete', $this->generateRoute(
             'open_orchestra_api_workflowfonction_delete',
-            array('WorkflowFonctionId' => $mixed->getId())
+            array('workflowFonctionId' => $mixed->getId())
         ));
         $facade->addLink('_self_form', $this->generateRoute(
             'open_orchestra_backoffice_workflowfonction_form',
-            array('WorkflowFonctionId' => $mixed->getId())
+            array('workflowFonctionId' => $mixed->getId())
         ));
 
         return $facade;
