29361fdf8fce05a788f9decb8316d7296792c204
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-06-05 16:51:44 +0200

    authorize underscore and dot character in list choice

--- BackofficeBundle/Form/DataTransformer/ChoicesOptionToArrayTransformer.php
+++ BackofficeBundle/Form/DataTransformer/ChoicesOptionToArrayTransformer.php
@@ -9,16 +9,6 @@ use Symfony\Component\Form\DataTransformerInterface;
  */
 class ChoicesOptionToArrayTransformer implements DataTransformerInterface
 {
-    protected $suppressSpecialCharacterClass;
-
-    /**
-     * @param SuppressSpecialCharacterTransformer $suppressSpecialCharacterClass
-     */
-    public function __construct(SuppressSpecialCharacterTransformer $suppressSpecialCharacterClass)
-    {
-        $this->suppressSpecialCharacterClass = $suppressSpecialCharacterClass;
-    }
-
     /**
      * @param array $arrayChoices
      *
@@ -48,7 +38,7 @@ class ChoicesOptionToArrayTransformer implements DataTransformerInterface
         $arrayChoice = array();
 
         foreach ($choices as $choice) {
-            $choice = $this->suppressSpecialCharacterClass->transform($choice);
+            $choice = $this->suppressSpecialCharacterTranslateChoice($choice);
             if ('' != $choice) {
                 $arrayChoice[$choice] = $choice;
             }
@@ -56,4 +46,27 @@ class ChoicesOptionToArrayTransformer implements DataTransformerInterface
 
         return $arrayChoice;
     }
+
+    /**
+     * @param string $choice
+     *
+     * @return string
+     */
+    protected function suppressSpecialCharacterTranslateChoice($choice)
+    {
+        $caracteres = array(
+            'À' => 'a', 'Á' => 'a', 'Â' => 'a', 'Ä' => 'a', 'à' => 'a', 'á' => 'a', 'â' => 'a', 'ä' => 'a', 'È' => 'e',
+            'É' => 'e', 'Ê' => 'e', 'Ë' => 'e', 'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'Ì' => 'i', 'Í' => 'i',
+            'Î' => 'i', 'Ï' => 'i', 'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'Ò' => 'o', 'Ó' => 'o', 'Ô' => 'o',
+            'Ö' => 'o', 'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'ö' => 'o', 'Ù' => 'u', 'Ú' => 'u', 'Û' => 'u', 'Ü' => 'u',
+            'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'Œ' => 'oe', 'œ' => 'oe'
+        );
+
+        $choice = strtr($choice, $caracteres);
+        $choice = preg_replace('#[^0-9a-z_.]+#i', '', $choice);
+
+        return $choice;
+    }
+
+
 }
