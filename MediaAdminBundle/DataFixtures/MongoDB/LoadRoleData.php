6329b01479da8ba0725749cf39b39e4c26b370be
Nicolas Thal <nico.th4l@gmail.com>
2015-06-22 16:32:53 +0200

    factorize role fixtures

--- MediaAdminBundle/DataFixtures/MongoDB/LoadRoleData.php
+++ MediaAdminBundle/DataFixtures/MongoDB/LoadRoleData.php
@@ -2,16 +2,14 @@
 
 namespace OpenOrchestra\MediaAdminBundle\DataFixtures\MongoDB;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use OpenOrchestra\MediaAdminBundle\LeftPanel\Strategies\TreeFolderPanelStrategy;
-use OpenOrchestra\ModelBundle\Document\Role;
-use OpenOrchestra\ModelBundle\Document\TranslatedValue;
+use OpenOrchestra\BackofficeBundle\DataFixtures\MongoDB\LoadRoleData as BaseLoadRoleData;
 
 /**
  * Class LoadRoleData
  */
-class LoadRoleData implements FixtureInterface
+class LoadRoleData extends BaseLoadRoleData
 {
     /**
      * @param ObjectManager $manager
@@ -21,39 +19,5 @@ class LoadRoleData implements FixtureInterface
         $manager->persist($this->generateRole(TreeFolderPanelStrategy::ROLE_ACCESS_TREE_FOLDER));
 
         $manager->flush();
-    }
-
-    /**
-     * @param string $roleName
-     *
-     * @return Role
-     */
-    protected function generateRole($roleName)
-    {
-        $role = new Role();
-        $role->setName($roleName);
-        $role->addDescription($this->generateTranslatedValue('en', $roleName));
-        $role->addDescription($this->generateTranslatedValue('fr', $roleName));
-        $role->addDescription($this->generateTranslatedValue('de', $roleName));
-        $role->addDescription($this->generateTranslatedValue('es', $roleName));
-
-        return $role;
-    }
-
-    /**
-     * Generate a translatedValue
-     *
-     * @param string $language
-     * @param string $value
-     *
-     * @return TranslatedValue
-     */
-    protected function generateTranslatedValue($language, $value)
-    {
-        $label = new TranslatedValue();
-        $label->setLanguage($language);
-        $label->setValue($value);
-
-        return $label;
     }
 }
