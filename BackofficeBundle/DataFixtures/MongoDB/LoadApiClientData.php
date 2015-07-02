85452d14c84b41410991597f086d68205513353d
Nicolas Thal <nico.th4l@gmail.com>
2015-07-02 11:39:43 +0200

    create baseApiModel bundle

--- BackofficeBundle/DataFixtures/MongoDB/LoadApiClientData.php
+++ BackofficeBundle/DataFixtures/MongoDB/LoadApiClientData.php
@@ -4,7 +4,7 @@ namespace OpenOrchestra\BackofficeBundle\DataFixtures\MongoDB;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
-use OpenOrchestra\BaseApiBundle\Document\ApiClient;
+use OpenOrchestra\BaseApiModelBundle\Document\ApiClient;
 
 /**
  * Class LoadApiClientData
