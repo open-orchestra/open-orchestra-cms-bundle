0ab9785980eb402cede25e21c9f4d8606526afb6
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-07-23 09:02:24 +0200

    add GridStack

--- GruntTasks/Options/concat.libcss.js
+++ GruntTasks/Options/concat.libcss.js
@@ -2,7 +2,8 @@ module.exports = {
     src: [
         'bower_components/jquery-ui/themes/base/base.css',
         'bower_components/jquery-ui/themes/base/theme.css',
-        'bower_components/datatables-bootstrap3/BS3/assets/css/datatables.css'
+        'bower_components/datatables-bootstrap3/BS3/assets/css/datatables.css',
+        'bower_components/gridstack/src/gridstack.css'
     ],
     dest: 'web/built/lib.css'
 };
