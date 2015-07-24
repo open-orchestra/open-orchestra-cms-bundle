5d960e21a3ea7d2df53066831b45fde64bdcedca
Nicolas Thal <nico.th4l@gmail.com>
2015-07-24 11:47:26 +0200

    add strategy for content list block

---
+++ Backoffice/BlockParameter/Strategies/ContentListStrategy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace OpenOrchestra\Backoffice\BlockParameter\Strategies;
+
+use OpenOrchestra\Backoffice\BlockParameter\BlockParameterInterface;
+use OpenOrchestra\DisplayBundle\DisplayBlock\Strategies\ContentListStrategy as BaseContentListStrategy;
+use OpenOrchestra\ModelInterface\Model\BlockInterface;
+
+/**
+ * Class ContentListStrategy
+ */
+class ContentListStrategy implements BlockParameterInterface
+{
+    /**
+     * @param BlockInterface $block
+     *
+     * @return bool
+     */
+    public function support(BlockInterface $block)
+    {
+        return BaseContentListStrategy::CONTENT_LIST == $block->getComponent();
+    }
+
+    /**
+     * @return array
+     */
+    public function getBlockParameter()
+    {
+        return array('request.aliasId');
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'content';
+    }
+}
