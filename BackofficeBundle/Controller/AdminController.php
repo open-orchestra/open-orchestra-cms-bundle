711adc715cc5364556af0e8b5a33d9c5f3ccd9ea
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-07-27 11:36:19 +0200

    update rename parameter  to  in admin controller

--- BackofficeBundle/Controller/AdminController.php
+++ BackofficeBundle/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace OpenOrchestra\BackofficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
 
@@ -15,20 +16,16 @@ class AdminController extends Controller
      * Back Office
      *
      * @param string|null $siteId
-     * @param string|null $language
      *
      * @Config\Route("", name="homepage")
-     * @Config\Route("/{siteId}/homepage/{language}", name="homepage_with_site_id_and_language")
+     * @Config\Route("/{siteId}/homepage/{_locale}", name="homepage_with_site_id_and_language")
      *
      * @return Response
      */
-    public function adminAction($siteId = null, $language = null)
+    public function adminAction($siteId = null)
     {
         $contextManager = $this->get('open_orchestra_backoffice.context_manager');
 
-        if ($language) {
-            $contextManager->setCurrentLocale($language);
-        }
         if ($siteId) {
             $site = $this->get('open_orchestra_model.repository.site')->findOneBySiteId($siteId);
             $contextManager->setCurrentsite($site->getSiteId(), $site->getName(), $site->getDefaultLanguage());
