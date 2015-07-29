12483038de90904a32691e3ca7b5049b29a6313b
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-07-29 14:30:55 +0200

    fix link smartadmin sound

--- GruntTasks/Options/concat.smartadminjs.js
+++ GruntTasks/Options/concat.smartadminjs.js
@@ -3,6 +3,7 @@ module.exports = {
         'bower_components/jquery/dist/jquery.js',
         'bower_components/jquery-ui/jquery-ui.js',
         'web/bundles/openorchestrabackoffice/smartadmin/js/app.config.js',
+        'web/bundles/openorchestrabackoffice/js/smartadminOrchestra.config.js',
         'bower_components/bootstrap/dist/js/bootstrap.js',
         'bower_components/jcrop/js/jquery.Jcrop.js',
         'bower_components/select2/select2.js',
