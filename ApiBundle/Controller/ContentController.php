14651759bfa9e22d2ff976f88c6fcc8b70060a5b
romain boessel <romain.boessel@businessecision.com>
2015-08-07 14:26:24 +0200

    add change for new version

--- ApiBundle/Controller/ContentController.php
+++ ApiBundle/Controller/ContentController.php
@@ -176,9 +176,9 @@ class ContentController extends BaseController
     public function duplicateAction(Request $request, $contentId)
     {
         /** @var ContentInterface $content */
-        $content = $this->findOneContent($contentId, $request->get('language'));
-        $newContent = $this->get('open_orchestra_backoffice.manager.content')->duplicateContent($content);
-
+        $content = $this->findOneContent($contentId, $request->get('language'), $request->get('version'));
+        $lastContent = $this->findOneContent($contentId, $request->get('language'));
+        $newContent = $this->get('open_orchestra_backoffice.manager.content')->duplicateContent($content, $lastContent);
 
         $em = $this->get('object_manager');
         $em->persist($newContent);
