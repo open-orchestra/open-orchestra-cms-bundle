a4d15af2352cc1f65ca6dfe86b123fd26bd06ef4
Nicolas Thal <nico.th4l@gmail.com>
2015-10-19 11:06:20 +0200

    add log roles

---
+++ LogBundle/DependencyInjection/Compiler/RoleCompilerPass.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace OpenOrchestra\LogBundle\DependencyInjection\Compiler;
+
+use OpenOrchestra\Backoffice\NavigationPanel\Strategies\AdministrationPanelStrategy;
+use OpenOrchestra\BackofficeBundle\DependencyInjection\Compiler\AbstractRoleCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class RoleCompilerPass
+ */
+class RoleCompilerPass extends AbstractRoleCompilerPass
+{
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     *
+     * @api
+     */
+    public function process(ContainerBuilder $container)
+    {
+        $this->addRoles($container, array(
+            AdministrationPanelStrategy::ROLE_ACCESS_LOG,
+        ));
+    }
+}
--- LogBundle/OpenOrchestraLogBundle.php
+++ LogBundle/OpenOrchestraLogBundle.php
@@ -2,8 +2,22 @@
 
 namespace OpenOrchestra\LogBundle;
 
+use OpenOrchestra\LogBundle\DependencyInjection\Compiler\RoleCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class OpenOrchestraLogBundle
+ */
 class OpenOrchestraLogBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RoleCompilerPass());
+    }
 }
