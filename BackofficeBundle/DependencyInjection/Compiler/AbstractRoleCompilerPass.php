035d5348feed1e5e46880154f82355a89a7852ce
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2015-10-30 11:35:24 +0100

    add role dynamicly in super admin

--- BackofficeBundle/DependencyInjection/Compiler/AbstractRoleCompilerPass.php
+++ BackofficeBundle/DependencyInjection/Compiler/AbstractRoleCompilerPass.php
@@ -2,6 +2,7 @@
 
 namespace OpenOrchestra\BackofficeBundle\DependencyInjection\Compiler;
 
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
@@ -21,8 +22,24 @@ abstract class AbstractRoleCompilerPass implements CompilerPassInterface
         }
 
         $definition = $container->getDefinition('open_orchestra_backoffice.collector.role');
+
         foreach ($roles as $role) {
             $definition->addMethodCall('addRole', array($role));
+        }
+
+        $this->addRoleHierarchieSuperAdmin($container, $roles);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     * @param array $roles
+     */
+    protected function addRoleHierarchieSuperAdmin(ContainerBuilder $container, array $roles)
+    {
+        $rolesHierarchy = $container->getParameter('security.role_hierarchy.roles');
+        if (isset($rolesHierarchy[UserInterface::ROLE_SUPER_ADMIN])) {
+            $rolesHierarchy[UserInterface::ROLE_SUPER_ADMIN] = array_merge($rolesHierarchy["ROLE_SUPER_ADMIN"], $roles);
+            $container->setParameter('security.role_hierarchy.roles', $rolesHierarchy);
         }
     }
 }
