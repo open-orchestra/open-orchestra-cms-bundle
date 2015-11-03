a3b4d30b9c6906aa7c8f6dbe589f79863e7491f6
Nicolas Thal <nico.th4l@gmail.com>
2015-11-03 11:30:46 +0100

    add document resolver

--- GroupBundle/OpenOrchestraGroupBundle.php
+++ GroupBundle/OpenOrchestraGroupBundle.php
@@ -1,6 +1,8 @@
 <?php
 namespace OpenOrchestra\GroupBundle;
 
+use OpenOrchestra\GroupBundle\DependencyInjection\Compiler\EntityResolverCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
@@ -8,5 +10,13 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class OpenOrchestraGroupBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
 
+        $container->addCompilerPass(new EntityResolverCompilerPass());
+    }
 }
