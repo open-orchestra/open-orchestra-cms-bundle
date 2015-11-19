984be3fccf51d82dbb49503f247a153ac94a860b
Nicolas Thal <nico.th4l@gmail.com>
2015-11-19 10:11:38 +0100

    use pagination repository instead of documentRepository

--- GroupBundle/Repository/GroupRepository.php
+++ GroupBundle/Repository/GroupRepository.php
@@ -2,13 +2,14 @@
 
 namespace OpenOrchestra\GroupBundle\Repository;
 
+use OpenOrchestra\BackofficeBundle\Repository\GroupRepositoryInterface;
 use OpenOrchestra\Pagination\MongoTrait\PaginationTrait;
 use OpenOrchestra\Repository\AbstractAggregateRepository;
 
 /**
  * Class GroupRepository
  */
-class GroupRepository extends AbstractAggregateRepository
+class GroupRepository extends AbstractAggregateRepository implements GroupRepositoryInterface
 {
     use PaginationTrait;
 }
