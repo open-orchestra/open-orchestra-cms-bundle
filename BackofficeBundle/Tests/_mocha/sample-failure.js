42474cee8b7c8e44c76a842085a639d82199bb47
Cédric ANNE <cedric.anne@businessdecision.com>
2015-11-24 09:38:40 +0100

    Fixed javascript unit test.

--- BackofficeBundle/Tests/_mocha/sample-failure.js
+++ BackofficeBundle/Tests/_mocha/sample-failure.js
@@ -1,5 +1,5 @@
-describe("Failure on test", function() {
-    it("should lead to the travis build failure", function() {
-        assert.equal(false, true);
+describe("Success on tests", function() {
+    it("should lead to the travis build success", function() {
+        assert.equal(true, true);
     });
 });
