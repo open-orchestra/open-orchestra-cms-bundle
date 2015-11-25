0bb3f50c71ed42d8c314a8348cd521f33ec8c462
Nicolas Thal <nico.th4l@gmail.com>
2015-11-25 14:53:19 +0100

    add content version voter

---
+++ Backoffice/Security/Authorization/Voter/ContentVersionVoter.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace OpenOrchestra\Backoffice\Security\Authorization\Voter;
+
+use OpenOrchestra\ModelInterface\Model\ContentInterface;
+use OpenOrchestra\ModelInterface\Repository\ContentRepositoryInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+/**
+ * Class ContentVersionVoter
+ */
+class ContentVersionVoter implements VoterInterface
+{
+    protected $repository;
+
+    /**
+     * @param ContentRepositoryInterface $repository
+     */
+    public function __construct(ContentRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Checks if the voter supports the given attribute.
+     *
+     * @param string $attribute An attribute
+     *
+     * @return bool true if this Voter supports the attribute, false otherwise
+     */
+    public function supportsAttribute($attribute)
+    {
+        return true;
+    }
+
+    /**
+     * Checks if the voter supports the given class.
+     *
+     * @param string $class A class name
+     *
+     * @return bool true if this Voter can process the class
+     */
+    public function supportsClass($class)
+    {
+        return $class instanceof ContentInterface;
+    }
+
+    /**
+     * Returns the vote for the given parameters.
+     *
+     * This method must return one of the following constants:
+     * ACCESS_GRANTED, ACCESS_DENIED, or ACCESS_ABSTAIN.
+     *
+     * @param TokenInterface $token A TokenInterface instance
+     * @param object|null $object The object to secure
+     * @param array $attributes An array of attributes associated with the method being invoked
+     *
+     * @return int either ACCESS_GRANTED, ACCESS_ABSTAIN, or ACCESS_DENIED
+     */
+    public function vote(TokenInterface $token, $object, array $attributes)
+    {
+        if (!$this->supportsClass($object)) {
+            return self::ACCESS_ABSTAIN;
+        }
+
+        $lastVersionContent = $this->repository->findOneByLanguage($object->getContentId(), $object->getLanguage());
+
+        if ($lastVersionContent instanceof ContentInterface && $object->getVersion() < $lastVersionContent->getVersion()) {
+            return self::ACCESS_DENIED;
+        }
+
+        return self::ACCESS_GRANTED;
+    }
+}
