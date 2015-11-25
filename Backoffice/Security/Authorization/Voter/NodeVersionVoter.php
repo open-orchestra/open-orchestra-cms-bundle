733194379abfebb2852ffaf6f759f5f57cb5550a
Nicolas Thal <nico.th4l@gmail.com>
2015-11-25 14:47:13 +0100

    add node version voter

---
+++ Backoffice/Security/Authorization/Voter/NodeVersionVoter.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OpenOrchestra\Backoffice\Security\Authorization\Voter;
+
+use OpenOrchestra\ModelInterface\Model\NodeInterface;
+use OpenOrchestra\ModelInterface\Repository\NodeRepositoryInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+/**
+ * Class NodeVersionVoter
+ */
+class NodeVersionVoter implements VoterInterface
+{
+    protected $repository;
+
+    /**
+     * @param NodeRepositoryInterface $repository
+     */
+    public function __construct(NodeRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Checks if the voter supports the given attribute.
+     *
+     * @param string $attribute An attribute
+     *
+     * @return bool true if this Voter supports the attribute, false otherwise
+     */
+    public function supportsAttribute($attribute)
+    {
+        return true;
+    }
+
+    /**
+     * Checks if the voter supports the given class.
+     *
+     * @param string $class A class name
+     *
+     * @return bool true if this Voter can process the class
+     */
+    public function supportsClass($class)
+    {
+        return $class instanceof NodeInterface && $class->getNodeType() !== NodeInterface::TYPE_TRANSVERSE;
+    }
+
+    /**
+     * Returns the vote for the given parameters.
+     *
+     * This method must return one of the following constants:
+     * ACCESS_GRANTED, ACCESS_DENIED, or ACCESS_ABSTAIN.
+     *
+     * @param TokenInterface $token A TokenInterface instance
+     * @param object|null $object The object to secure
+     * @param array $attributes An array of attributes associated with the method being invoked
+     *
+     * @return int either ACCESS_GRANTED, ACCESS_ABSTAIN, or ACCESS_DENIED
+     */
+    public function vote(TokenInterface $token, $object, array $attributes)
+    {
+        if (!$this->supportsClass($object)) {
+            return self::ACCESS_ABSTAIN;
+        }
+
+        $lastVersionNode = $this->repository->findInLastVersion(
+            $object->getNodeId(),
+            $object->getLanguage(),
+            $object->getSiteId()
+        );
+
+        if ($lastVersionNode instanceof NodeInterface && $object->getVersion() < $lastVersionNode->getVersion()) {
+            return self::ACCESS_DENIED;
+        }
+
+        return self::ACCESS_GRANTED;
+    }
+}
