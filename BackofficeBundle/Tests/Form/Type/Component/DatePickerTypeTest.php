c7a64257a0b30c8a6e04c2b3b03caa8ae141f961
Nicolas ANNE <nicolas.anne@businessdecision.com>
2015-12-15 16:17:00 +0100

    add test

---
+++ BackofficeBundle/Tests/Form/Type/Component/DatePickerTypeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace OpenOrchestra\BackofficeBundle\Tests\Form\Type\Component;
+
+use OpenOrchestra\BackofficeBundle\Form\Type\Component\DatePickerType;
+use Phake;
+
+/**
+ * Class DatePickerTypeTest
+ */
+class DatePickerTypeTest extends \PHPUnit_Framework_TestCase
+{
+    protected $form;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->form = new DatePickerType();
+    }
+
+    /**
+     * test Instance
+     */
+    public function testGetInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\Form\Extension\Core\Type\DateType', $this->form);
+    }
+
+    /**
+     * test Name
+     */
+    public function testGetName()
+    {
+        $this->assertEquals('oo_date_picker', $this->form->getName());
+    }
+}
