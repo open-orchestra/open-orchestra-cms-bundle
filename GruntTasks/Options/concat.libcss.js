9d1d3dfb92906987a5d01e4e2694186bd9fc282a
itkg-ngilain <noel.gilain@businessdecision.com>
2016-01-12 12:39:48 +0100

    Change grunt tasks to restore IE9 compatibility

--- GruntTasks/Options/concat.libcss.js
+++ GruntTasks/Options/concat.libcss.js
@@ -1,11 +1,12 @@
 module.exports = {
     src: [
-        'vendor/bower_components/jquery-ui/themes/base/base.css',
-        'vendor/bower_components/jquery-ui/themes/base/theme.css',
-        'vendor/bower_components/datatables.net-bs/css/dataTables.bootstrap.css',
-        'vendor/bower_components/datatables.net-buttons-bs/js/buttons.bootstrap.css',
-        'vendor/bower_components/gridstack/src/gridstack.css',
-        'vendor/bower_components/gridstack/src/gridstack-extra.css'
+       'bower_components/bootstrap/dist/css/bootstrap.css',
+       'vendor/bower_components/open-sans/css/open-sans.css',
+       'vendor/bower_components/jquery-minicolors/jquery.minicolors.css',
+       'web/bundles/openorchestrabackoffice/smartadmin/css/bootstrap.css',
+       'vendor/bower_components/font-awesome/css/font-awesome.css',
+       'vendor/bower_components/datatables.net-bs/css/dataTables.bootstrap.css',
+       'vendor/bower_components/datatables.net-buttons-bs/js/buttons.bootstrap.css'
     ],
     dest: 'web/built/lib.css'
 };
--- GruntTasks/Options/concat.smartadminpatchescss.js
+++ GruntTasks/Options/concat.smartadminpatchescss.js
@@ -1,17 +1,5 @@
 module.exports = {
     src: [
-
-        //--[ SMARTADMIN ]--//
-//        'bower_components/bootstrap/dist/css/bootstrap.css',
-       'vendor/bower_components/open-sans/css/open-sans.css',
-       'vendor/bower_components/jquery-minicolors/jquery.minicolors.css',
-       'web/bundles/openorchestrabackoffice/smartadmin/css/bootstrap.css',
-       'vendor/bower_components/font-awesome/css/font-awesome.css',
-       'web/bundles/openorchestrabackoffice/smartadmin/css/smartadmin-production-plugins.css',
-       'web/bundles/openorchestrabackoffice/smartadmin/css/smartadmin-production.css',
-       'web/bundles/openorchestrabackoffice/smartadmin/css/smartadmin-skins.css',
-       'web/bundles/openorchestrabackoffice/smartadmin/css/smartadmin-rtl.css',
-
        //--[ ORCHESTRA SMARTADMIN PATCHES ]--//
        'web/built/openorchestrabackoffice/css/smartadminPatches/flags.css',
        'web/built/openorchestrabackoffice/css/smartadminPatches/title.css',
@@ -31,5 +19,5 @@ module.exports = {
        //--[ TINYMCE PATCHES ]--//
        'web/built/openorchestrabackoffice/css/tinyMCEPatches/floatPanel.css'
     ],
-    dest: 'web/built/smartadmin.css'
+    dest: 'web/built/smartadminpatches.css'
 };
