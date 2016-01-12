eff4f29e1f24381671cd238b76566689222cfa60
itkg-ngilain <noel.gilain@businessdecision.com>
2016-01-12 14:41:11 +0100

    Remove multiple bootstrap loading

--- GruntTasks/Options/concat.libcss.js
+++ GruntTasks/Options/concat.libcss.js
@@ -1,9 +1,8 @@
 module.exports = {
     src: [
-       'bower_components/bootstrap/dist/css/bootstrap.css',
+       'vendor/bower_components/bootstrap/dist/css/bootstrap.css',
        'vendor/bower_components/open-sans/css/open-sans.css',
        'vendor/bower_components/jquery-minicolors/jquery.minicolors.css',
-       'web/bundles/openorchestrabackoffice/smartadmin/css/bootstrap.css',
        'vendor/bower_components/font-awesome/css/font-awesome.css',
        'vendor/bower_components/datatables.net-bs/css/dataTables.bootstrap.css',
        'vendor/bower_components/datatables.net-buttons-bs/js/buttons.bootstrap.css'
