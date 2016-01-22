d45f2de466fb31ca19e1de01f29d6433cde1f825
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-01-22 11:08:11 +0100

    remove comment lines

--- BackofficeBundle/Tests/_mocha/OrchestraBORouter/OrchestraBORouter-test.js
+++ BackofficeBundle/Tests/_mocha/OrchestraBORouter/OrchestraBORouter-test.js
@@ -17,13 +17,3 @@ describe("OrchestraBORouter", function() {
         assert.equal(router.routePatterns[routeName], routePattern);
     });
 });
-/*
-
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            chai.assert.equal(-1, [1,2,3].indexOf(5));
-            chai.assert.equal(-1, [1,2,3].indexOf(0));
-        });
-    });
-});*/
