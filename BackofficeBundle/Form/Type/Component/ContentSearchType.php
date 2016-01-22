fe4630bda17d3d2cab38a11573d02f870b1c6fb6
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-01-22 12:17:28 +0100

    use LuceneToBddTransformer

--- BackofficeBundle/Form/Type/Component/ContentSearchType.php
+++ BackofficeBundle/Form/Type/Component/ContentSearchType.php
@@ -4,26 +4,41 @@ namespace OpenOrchestra\BackofficeBundle\Form\Type\Component;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use OpenOrchestra\Transformer\LuceneToBddTransformerInterface;
 
 /**
  * Class ContentSearchType
  */
 class ContentSearchType extends AbstractType
 {
+    protected $transformer;
+
+    /**
+     * @param LuceneToBddTransformerInterface $transformer
+     */
+    public function __construct(LuceneToBddTransformerInterface $transformer)
+    {
+        $this->transformer = $transformer;
+        $this->transformer->setField('keywords');
+    }
+
     /**
      * @param FormBuilderInterface $builder
      * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+        $builder->addModelTransformer($this->transformer);
+
         $builder->add('contentType', 'oo_content_type_choice', array(
             'label' => 'open_orchestra_backoffice.form.content_search.content_type',
             'required' => false
         ));
-        $builder->add('choiceType', 'oo_operator_choice', array(
-            'label' => 'open_orchestra_backoffice.form.content_search.choice_type',
-        ));
         $builder->add('keywords', 'oo_keywords_choice', array(
+            'attr' => array(
+                'class' => 'select2-lucene',
+            ),
             'embedded' => false,
             'required' => false,
             'label' => 'open_orchestra_backoffice.form.content_search.content_keyword',
