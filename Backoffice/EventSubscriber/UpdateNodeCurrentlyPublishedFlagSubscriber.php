867045b81bbe99ad98eb54cb12089f8e7f7fac4b
Nicolas Thal <nico.th4l@gmail.com>
2016-02-26 16:52:53 +0100

    add listener

---
+++ Backoffice/EventSubscriber/UpdateNodeCurrentlyPublishedFlagSubscriber.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace OpenOrchestra\Backoffice\EventSubscriber;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use OpenOrchestra\Backoffice\Manager\NodeManager;
+use OpenOrchestra\ModelInterface\Event\NodeEvent;
+use OpenOrchestra\ModelInterface\Model\NodeInterface;
+use OpenOrchestra\ModelInterface\NodeEvents;
+use OpenOrchestra\ModelInterface\Repository\NodeRepositoryInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class UpdateNodeCurrentlyPublishedFlagSubscriber
+ */
+class UpdateNodeCurrentlyPublishedFlagSubscriber implements EventSubscriberInterface
+{
+    protected $nodeRepository;
+    protected $objectManager;
+
+    /**
+     * @param NodeRepositoryInterface $nodeRepository
+     * @param ObjectManager           $objectManager
+     */
+    public function __construct(NodeRepositoryInterface $nodeRepository, ObjectManager $objectManager)
+    {
+        $this->nodeRepository = $nodeRepository;
+        $this->objectManager = $objectManager;
+    }
+
+    /**
+     * @param NodeEvent $event
+     */
+    public function updateFlag(NodeEvent $event)
+    {
+        $node = $event->getNode();
+
+        if ($node->getStatus()->isPublished()) {
+            $lastPublishedNode = $this->nodeRepository->findPublishedInLastVersion($node->getNodeId(), $node->getLanguage(), $node->getSiteId());
+            if (!($lastPublishedNode instanceof NodeInterface) || $lastPublishedNode->getVersion() <= $node->getVersion()) {
+                $this->updatePublishedFlag($node);
+            }
+        } elseif ($node->isCurrentlyPublished()) {
+            $lastPublishedNode = $this->nodeRepository->findPublishedInLastVersionWithoutFlag($node->getNodeId(), $node->getLanguage(), $node->getSiteId());
+            if ($lastPublishedNode instanceof NodeInterface && $lastPublishedNode->getVersion() < $node->getVersion()) {
+                $this->updatePublishedFlag($lastPublishedNode);
+            } else {
+                $node->setCurrentlyPublished(false);
+                $this->objectManager->flush($node);
+            }
+        }
+    }
+
+    /**
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            NodeEvents::NODE_CHANGE_STATUS => array('updateFlag', 100),
+        );
+    }
+
+    /**
+     * @param NodeInterface $node
+     */
+    protected function updatePublishedFlag(NodeInterface $node)
+    {
+        $publishedNodes = $this->nodeRepository->findCurrentlyPublished($node->getNodeId(), $node->getLanguage(), $node->getSiteId());
+        /** @var NodeInterface $publishedNode */
+        foreach ($publishedNodes as $publishedNode) {
+            $publishedNode->setCurrentlyPublished(false);
+            $this->objectManager->flush($publishedNode);
+        }
+
+        $node->setCurrentlyPublished(true);
+        $this->objectManager->flush($node);
+    }
+}
---
+++ Backoffice/Tests/EventSubscriber/UpdateNodeCurrentlyPublishedFlagSubscriberTest.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace OpenOrchestra\Backoffice\Tests\EventSubscriber;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use OpenOrchestra\Backoffice\EventSubscriber\UpdateNodeCurrentlyPublishedFlagSubscriber;
+use OpenOrchestra\ModelInterface\Event\NodeEvent;
+use OpenOrchestra\ModelInterface\Model\NodeInterface;
+use OpenOrchestra\ModelInterface\Model\StatusInterface;
+use OpenOrchestra\ModelInterface\NodeEvents;
+use OpenOrchestra\ModelInterface\Repository\NodeRepositoryInterface;
+use Phake;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Test UpdateNodeCurrentlyPublishedFlagSubscriberTest
+ */
+class UpdateNodeCurrentlyPublishedFlagSubscriberTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UpdateNodeCurrentlyPublishedFlagSubscriber
+     */
+    protected $subscriber;
+
+    protected $nodeRepository;
+    protected $objectManager;
+    protected $event;
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->nodeRepository = Phake::mock(NodeRepositoryInterface::CLASS);
+        Phake::when($this->nodeRepository)->findCurrentlyPublished(Phake::anyParameters())->thenReturn(array());
+
+        $this->objectManager = Phake::mock(ObjectManager::CLASS);
+        $this->event = Phake::mock(NodeEvent::CLASS);
+
+        $this->subscriber = new UpdateNodeCurrentlyPublishedFlagSubscriber($this->nodeRepository, $this->objectManager);
+    }
+
+    /**
+     * Test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf(EventSubscriberInterface::CLASS, $this->subscriber);
+    }
+
+    /**
+     * Test subscribed events
+     */
+    public function testSubscribedEvent()
+    {
+        $this->assertSame(array(
+            NodeEvents::NODE_CHANGE_STATUS => array('updateFlag', 100)),
+            $this->subscriber->getSubscribedEvents()
+        );
+    }
+
+    /**
+     * @param $repositoryCall
+     * @param $managerCall
+     * @param $statusPublished
+     * @param $nodeVersion
+     * @param $publishedFlag
+     * @param $lastPublishedNodeVersion
+     * @param $previousStatusPublished
+     *
+     * @dataProvider provideUpdateFlagData
+     */
+    public function testUpdateFlag($repositoryCall, $managerCall, $lastPublishedCall, $statusPublished, $nodeVersion, $publishedFlag, $lastPublishedNodeVersion, $previousStatusPublished)
+    {
+        $status = Phake::mock(StatusInterface::CLASS);
+        Phake::when($status)->isPublished()->thenReturn($statusPublished);
+        $node = Phake::mock(NodeInterface::CLASS);
+        Phake::when($node)->getStatus()->thenReturn($status);
+        Phake::when($node)->getVersion()->thenReturn($nodeVersion);
+        Phake::when($node)->isCurrentlyPublished()->thenReturn($publishedFlag);
+        Phake::when($this->event)->getNode()->thenReturn($node);
+
+        $lastPublishedNode = Phake::mock(NodeInterface::CLASS);
+        Phake::when($lastPublishedNode)->getVersion()->thenReturn($lastPublishedNodeVersion);
+        Phake::when($this->nodeRepository)->findPublishedInLastVersion(Phake::anyParameters())->thenReturn($lastPublishedNode);
+        Phake::when($this->nodeRepository)->findPublishedInLastVersionWithoutFlag(Phake::anyParameters())->thenReturn($lastPublishedNode);
+
+        $previousStatus = Phake::mock(StatusInterface::CLASS);
+        Phake::when($previousStatus)->isPublished()->thenReturn($previousStatusPublished);
+        Phake::when($this->event)->getPreviousStatus()->thenReturn($previousStatus);
+
+        $wrongPublishedNode = Phake::mock(NodeInterface::CLASS);
+        Phake::when($this->nodeRepository)->findCurrentlyPublished(Phake::anyParameters())->thenReturn(array($wrongPublishedNode, $wrongPublishedNode));
+
+        $this->subscriber->updateFlag($this->event);
+
+        Phake::verify($this->nodeRepository, Phake::times($repositoryCall))->findPublishedInLastVersion(Phake::anyParameters());
+        Phake::verify($this->nodeRepository, Phake::times($lastPublishedCall))->findPublishedInLastVersionWithoutFlag(Phake::anyParameters());
+        Phake::verify($this->objectManager, Phake::times($managerCall))->flush($node);
+        Phake::verify($this->objectManager, Phake::times($lastPublishedCall))->flush($lastPublishedNode);
+        if ($statusPublished && $nodeVersion >= $lastPublishedNodeVersion) {
+            Phake::verify($wrongPublishedNode, Phake::times(2))->setCurrentlyPublished(false);
+            Phake::verify($this->objectManager, Phake::times(2))->flush($wrongPublishedNode);
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function provideUpdateFlagData()
+    {
+        return array(
+            'nothing published' => array(0, 0, 0, false, 4, false, 2, false),
+            'publish newer node' => array(1, 1, 0, true, 2, false, 1, false),
+            'publish older node' => array(1, 0, 0, true, 1, false, 2, false),
+            'unpublish older node' => array(0, 0, 0, false, 1, false, 2, true),
+            'unpublish last node' => array(0, 0, 1, false, 2, true, 1, true),
+        );
+    }
+
+    /**
+     * Test update node with no previous published version
+     */
+    public function testUpdateFlagWithNoPreviousPublishedNode()
+    {
+        $status = Phake::mock(StatusInterface::CLASS);
+        Phake::when($status)->isPublished()->thenReturn(true);
+        $node = Phake::mock(NodeInterface::CLASS);
+        Phake::when($node)->getStatus()->thenReturn($status);
+        Phake::when($node)->getVersion()->thenReturn(1);
+        Phake::when($node)->isCurrentlyPublished()->thenReturn(false);
+        Phake::when($this->event)->getNode()->thenReturn($node);
+
+        $previousStatus = Phake::mock(StatusInterface::CLASS);
+        Phake::when($previousStatus)->isPublished()->thenReturn(false);
+        Phake::when($this->event)->getPreviousStatus()->thenReturn($previousStatus);
+
+        $this->subscriber->updateFlag($this->event);
+
+        Phake::verify($this->nodeRepository)->findPublishedInLastVersion(Phake::anyParameters());
+        Phake::verify($node)->setCurrentlyPublished(true);
+        Phake::verify($this->objectManager)->flush($node);
+    }
+
+    public function testUpdateFlagWhenUnpblishNodeAndNoOtherPublishedNode()
+    {
+        $status = Phake::mock(StatusInterface::CLASS);
+        Phake::when($status)->isPublished()->thenReturn(false);
+        $node = Phake::mock(NodeInterface::CLASS);
+        Phake::when($node)->getStatus()->thenReturn($status);
+        Phake::when($node)->getVersion()->thenReturn(2);
+        Phake::when($node)->isCurrentlyPublished()->thenReturn(true);
+        Phake::when($this->event)->getNode()->thenReturn($node);
+
+        $previousStatus = Phake::mock(StatusInterface::CLASS);
+        Phake::when($previousStatus)->isPublished()->thenReturn(true);
+        Phake::when($this->event)->getPreviousStatus()->thenReturn($previousStatus);
+
+        $wrongPublishedNode = Phake::mock(NodeInterface::CLASS);
+        Phake::when($this->nodeRepository)->findCurrentlyPublished(Phake::anyParameters())->thenReturn(array($wrongPublishedNode, $wrongPublishedNode));
+
+        $this->subscriber->updateFlag($this->event);
+
+        Phake::verify($this->nodeRepository, Phake::never())->findPublishedInLastVersion(Phake::anyParameters());
+        Phake::verify($this->nodeRepository)->findPublishedInLastVersionWithoutFlag(Phake::anyParameters());
+        Phake::verify($node)->setCurrentlyPublished(false);
+        Phake::verify($this->objectManager)->flush($node);
+    }
+}
