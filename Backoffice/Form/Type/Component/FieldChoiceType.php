cd5bd4ed4a369e8ca0b022216f56a7c69a9ef918
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-03-09 16:51:36 +0100

    fix choice field with multiple option

--- Backoffice/Form/Type/Component/FieldChoiceType.php
+++ Backoffice/Form/Type/Component/FieldChoiceType.php
@@ -2,41 +2,29 @@
 
 namespace OpenOrchestra\Backoffice\Form\Type\Component;
 
-use OpenOrchestra\Backoffice\Form\DataTransformer\ChoiceArrayToStringTransformer;
-use OpenOrchestra\Backoffice\Form\DataTransformer\ChoiceStringToArrayTransformer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 /**
  * Class FieldChoiceType
  */
 class FieldChoiceType extends AbstractType
 {
-    protected $choiceArrayToStringTransformer;
-
-    /**
-     * @param ChoiceArrayToStringTransformer $choiceArrayToStringTransformer
-     * @param ChoiceStringToArrayTransformer $choiceStringToArrayTransformer
-     */
-    public function __construct(
-        ChoiceArrayToStringTransformer $choiceArrayToStringTransformer,
-        ChoiceStringToArrayTransformer $choiceStringToArrayTransformer
-    )
-    {
-        $this->choiceArrayToStringTransformer = $choiceArrayToStringTransformer;
-        $this->choiceStringToArrayTransformer = $choiceStringToArrayTransformer;
-    }
-
     /**
      * @param FormBuilderInterface $builder
      * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        if ($options['multiple'] === false) {
-            $builder->addModelTransformer($this->choiceArrayToStringTransformer);
-        } else {
-            $builder->addModelTransformer($this->choiceStringToArrayTransformer);
+        if (false === $options['multiple']) {
+            $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                $data = $event->getData();
+                if (is_array($data) && $data !== array()) {
+                    $event->setData(current($data));
+                }
+            }, 1);
         }
     }
 
