6bf3e2ac29cc632aca0c80be1017427b0c9997f1
KevinArtus <kevinar9@hotmail.fr>
2016-04-13 15:34:13 +0200

    Change test node with new roles order

--- BackofficeBundle/Tests/Functional/Controller/NodeControllerTest.php
+++ BackofficeBundle/Tests/Functional/Controller/NodeControllerTest.php
@@ -123,8 +123,8 @@ class NodeControllerTest extends AbstractControllerTest
         $this->assertEquals(1, count($this->redirectionRepository->findAll()));
         $routeDocumentCount = count($this->routeDocumentRepository->findAll());
 
-        $this->changeNodeStatusWithRouteRedirectionTest($nodeId, $statuses[1], 1, $routeDocumentCount);
-        $this->changeNodeStatusWithRouteRedirectionTest($nodeId, $statuses[2], 1, $routeDocumentCount + 3);
+        $this->changeNodeStatusWithRouteRedirectionTest($nodeId, $statuses[2], 1, $routeDocumentCount);
+        $this->changeNodeStatusWithRouteRedirectionTest($nodeId, $statuses[1], 1, $routeDocumentCount + 3);
 
         $this->client->request('POST', '/api/node/' . $nodeName . '/duplicate/1?language=' . $node->language, array());
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
@@ -135,8 +135,8 @@ class NodeControllerTest extends AbstractControllerTest
         $this->documentManager->persist($newNode);
         $this->documentManager->flush($newNode);
 
-        $this->changeNodeStatusWithRouteRedirectionTest($newNode->getId(), $statuses[1], 1, $routeDocumentCount + 3);
-        $this->changeNodeStatusWithRouteRedirectionTest($newNode->getId(), $statuses[2], 4, $routeDocumentCount + 12);
+        $this->changeNodeStatusWithRouteRedirectionTest($newNode->getId(), $statuses[2], 1, $routeDocumentCount + 3);
+        $this->changeNodeStatusWithRouteRedirectionTest($newNode->getId(), $statuses[1], 4, $routeDocumentCount + 12);
         $this->changeNodeStatusWithRouteRedirectionTest($newNode->getId(), $statuses[0], 1, $routeDocumentCount + 3);
         $this->changeNodeStatusWithRouteRedirectionTest($nodeId, $statuses[0], 1, $routeDocumentCount);
 
