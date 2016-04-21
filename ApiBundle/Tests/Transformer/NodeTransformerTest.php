323679dd8c306b380fddc830ef834d5b689bdbb4
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-04-21 17:42:55 +0200

    fix update status role update other type node

--- ApiBundle/Tests/Transformer/NodeTransformerTest.php
+++ ApiBundle/Tests/Transformer/NodeTransformerTest.php
@@ -117,6 +117,36 @@ class NodeTransformerTest extends AbstractBaseTestCase
     }
 
     /**
+     * Test transform with update not granted
+     */
+    public function testTransformNotTransverseNotGranted()
+    {
+        $facade = Phake::mock('OpenOrchestra\BaseApi\Facade\FacadeInterface');
+        Phake::when($this->authorizationChecker)->isGranted(Phake::anyParameters())->thenReturn(false);
+
+        Phake::when($this->transformer)->transform(Phake::anyParameters())->thenReturn($facade);
+        $area = Phake::mock('OpenOrchestra\ModelInterface\Model\AreaInterface');
+        $areas = new ArrayCollection();
+        $areas->add($area);
+
+        Phake::when($this->node)->getAreas()->thenReturn($areas);
+        $facade = $this->nodeTransformer->transform($this->node);
+
+        $this->assertSame($facade->editable, false);
+        $this->assertInstanceOf('OpenOrchestra\ApiBundle\Facade\NodeFacade', $facade);
+        $this->assertArrayHasKey('_self_form', $facade->getLinks());
+        $this->assertArrayHasKey('_self_version', $facade->getLinks());
+        $this->assertArrayHasKey('_language_list', $facade->getLinks());
+        $this->assertArrayNotHasKey('_self_duplicate', $facade->getLinks());
+        $this->assertArrayNotHasKey('_self_status_change', $facade->getLinks());
+        $this->assertArrayNotHasKey('_self_delete', $facade->getLinks());
+        $this->assertArrayHasKey('_block_list', $facade->getLinks());
+        Phake::verify($this->router, Phake::times(8))->generate(Phake::anyParameters());
+        Phake::verify($this->transformer)->transform($area, $this->node);
+        Phake::verify($this->siteRepository)->findOneBySiteId(Phake::anyParameters());
+    }
+
+    /**
      * Test transform
      */
     public function testTransformTransverse()
