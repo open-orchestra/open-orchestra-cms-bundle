8185fdd7dca1d0d6470edf6b2ee3b4c9471938d1
Ali mbecheze Oumar <benomar01@gmail.com>
2016-05-06 10:13:55 +0200

    fix dpendance bundle

--- GroupBundle/DataFixtures/MongoDB/LoadGroupDemoData.php
+++ GroupBundle/DataFixtures/MongoDB/LoadGroupDemoData.php
@@ -6,10 +6,8 @@ use Doctrine\Common\Persistence\ObjectManager;
 use OpenOrchestra\Backoffice\NavigationPanel\Strategies\AdministrationPanelStrategy;
 use OpenOrchestra\Backoffice\NavigationPanel\Strategies\TreeNodesPanelStrategy;
 use OpenOrchestra\LogBundle\NavigationPanel\Strategies\LogPanelStrategy;
-use OpenOrchestra\MediaAdminBundle\NavigationPanel\Strategies\TreeFolderPanelStrategy;
 use OpenOrchestra\ModelInterface\DataFixtures\OrchestraProductionFixturesInterface;
 use OpenOrchestra\ModelInterface\DataFixtures\OrchestraFunctionalFixturesInterface;
-use OpenOrchestra\WorkflowFunctionAdminBundle\NavigationPanel\Strategies\WorkflowFunctionPanelStrategy;
 
 /**
  * Class LoadGroupDemoData
@@ -24,29 +22,18 @@ class LoadGroupDemoData extends AbstractLoadGroupData implements OrchestraProduc
         $group2 = $this->generateGroup('Demo group', 'Demo group', 'Groupe de démo', 'site2', 'group2');
         $manager->persist($group2);
 
-        $adminGroup = $this->generateGroup('Admin group', 'Admin group', 'Groupe admin');
+        $adminGroup = $this->generateGroup('Admin group', 'Admin group', 'Groupe admin', null, 'group-admin');
         $adminGroup->addRole(AdministrationPanelStrategy::ROLE_ACCESS_REMOVED_TRASHCAN);
         $adminGroup->addRole(AdministrationPanelStrategy::ROLE_ACCESS_THEME);
         $adminGroup->addRole(AdministrationPanelStrategy::ROLE_ACCESS_CREATE_THEME);
         $adminGroup->addRole(AdministrationPanelStrategy::ROLE_ACCESS_UPDATE_THEME);
         $adminGroup->addRole(AdministrationPanelStrategy::ROLE_ACCESS_DELETE_THEME);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_CREATE_MEDIA);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_CREATE_MEDIA_FOLDER);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_DELETE_MEDIA);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_DELETE_MEDIA_FOLDER);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_MEDIA_FOLDER);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_UPDATE_MEDIA);
-        $adminGroup->addRole(TreeFolderPanelStrategy::ROLE_ACCESS_UPDATE_MEDIA_FOLDER);
-        $adminGroup->addRole(WorkflowFunctionPanelStrategy::ROLE_ACCESS_WORKFLOWFUNCTION);
-        $adminGroup->addRole(WorkflowFunctionPanelStrategy::ROLE_ACCESS_CREATE_WORKFLOWFUNCTION);
-        $adminGroup->addRole(WorkflowFunctionPanelStrategy::ROLE_ACCESS_UPDATE_WORKFLOWFUNCTION);
-        $adminGroup->addRole(WorkflowFunctionPanelStrategy::ROLE_ACCESS_DELETE_WORKFLOWFUNCTION);
         $adminGroup->addRole(LogPanelStrategy::ROLE_ACCESS_LOG);
         $adminGroup->addRole(TreeNodesPanelStrategy::ROLE_ACCESS_ERROR_NODE);
         $adminGroup->addRole(TreeNodesPanelStrategy::ROLE_ACCESS_CREATE_ERROR_NODE);
         $adminGroup->addRole(TreeNodesPanelStrategy::ROLE_ACCESS_UPDATE_ERROR_NODE);
-        $manager->persist($adminGroup);
 
+        $manager->persist($adminGroup);
         $manager->flush();
     }
 
