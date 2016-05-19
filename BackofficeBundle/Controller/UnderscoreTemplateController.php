0703a68a632fc2317fe8edb33583ab86ec81d45e
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-05-19 17:37:12 +0200

    fix load tempalte underscore wiithout session

--- BackofficeBundle/Controller/UnderscoreTemplateController.php
+++ BackofficeBundle/Controller/UnderscoreTemplateController.php
@@ -5,6 +5,7 @@ namespace OpenOrchestra\BackofficeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class UnderscoreTemplateController
@@ -21,9 +22,13 @@ class UnderscoreTemplateController extends Controller
      */
     public function showAction(Request $request)
     {
+        $response = new Response();
+        $response->headers->set('template-id', $request->get('templateId'));
+
         return $this->render(
             $request->get('templateId') . '._tpl.twig',
-            array('language' => $request->get('language'))
+            array('language' => $request->get('language')),
+            $response
         );
     }
 }
