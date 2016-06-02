061058725abac40ea02f01a0990bb26a3894a38d
Ali mbecheze Oumar <benomar01@gmail.com>
2016-06-02 14:11:26 +0200

    make meta keywords and descriptions multilanguage in site and note

--- ApiBundle/Transformer/SiteTransformer.php
+++ ApiBundle/Transformer/SiteTransformer.php
@@ -2,17 +2,35 @@
 
 namespace OpenOrchestra\ApiBundle\Transformer;
 
+use OpenOrchestra\Backoffice\Manager\TranslationChoiceManager;
 use OpenOrchestra\BaseApi\Exceptions\TransformerParameterTypeException;
 use OpenOrchestra\Backoffice\NavigationPanel\Strategies\AdministrationPanelStrategy;
 use OpenOrchestra\BaseApi\Facade\FacadeInterface;
 use OpenOrchestra\BaseApi\Transformer\AbstractSecurityCheckerAwareTransformer;
 use OpenOrchestra\ModelInterface\Model\SiteInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * Class SiteTransformer
  */
 class SiteTransformer extends AbstractSecurityCheckerAwareTransformer
 {
+    protected $translationChoiceManager;
+
+    /**
+     * @param string                        $facadeClass
+     * @param AuthorizationCheckerInterface $authorizationChecker
+     * @param TranslationChoiceManager      $translationChoiceManager
+     */
+    public function __construct(
+        $facadeClass,
+        AuthorizationCheckerInterface $authorizationChecker,
+        TranslationChoiceManager $translationChoiceManager
+    ){
+        parent::__construct($facadeClass, $authorizationChecker);
+        $this->translationChoiceManager = $translationChoiceManager;
+    }
+    
     /**
      * @param SiteInterface $site
      *
@@ -31,8 +49,8 @@ class SiteTransformer extends AbstractSecurityCheckerAwareTransformer
         $facade->id = $site->getId();
         $facade->siteId = $site->getSiteId();
         $facade->name = $site->getName();
-        $facade->metaKeywords = $site->getMetaKeywords();
-        $facade->metaDescription = $site->getMetaDescription();
+        $facade->metaKeyword = $this->translationChoiceManager->choose($site->getMetaKeywords());
+        $facade->metaDescription = $this->translationChoiceManager->choose($site->getMetaDescriptions());
         $facade->metaIndex = $site->getMetaIndex();
         $facade->metaFollow = $site->getMetaFollow();
         $facade->theme = $this->getTransformer('theme')->transform($site->getTheme());
