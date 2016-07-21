63e5a529548d8ea3af5d89f2704dbb005913b263
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-07-21 11:20:18 +0200

    remove unused use

--- BackofficeBundle/Controller/InternalLinkController.php
+++ BackofficeBundle/Controller/InternalLinkController.php
@@ -6,8 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration as Config;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
-use Symfony\Component\HttpFoundation\ParameterBag;
-
 /**
  * Class InternalLinkController
  */
