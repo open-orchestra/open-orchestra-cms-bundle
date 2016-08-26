50a0d98b7f44e42ef956a0c4a4a694a11b367a27
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-08-26 11:46:01 +0200

    fix conflict site subscriber

--- Backoffice/EventSubscriber/SiteSubscriber.php
+++ Backoffice/EventSubscriber/SiteSubscriber.php
@@ -53,43 +53,6 @@ class SiteSubscriber implements EventSubscriberInterface
     }
 
     /**
-     * @param FormEvent $event
-     */
-    protected function addFormElements(FormEvent $event)
-    {
-        $form = $event->getForm();
-        $data = $event->getData();
-
-        $siteId = is_array($data) && array_key_exists('siteId', $data) ? $data['siteId'] : $form->get('siteId')->getConfig()->getEmptyData();
-        $nodeId = is_array($data) && array_key_exists('nodeId', $data) ? $data['nodeId'] : '';
-        $aliasId = is_array($data) && array_key_exists('aliasId', $data) ? $data['aliasId'] : '';
-
-        if ($siteId != '') {
-            $form->add('nodeId', 'oo_node_choice', array(
-                'label' => 'open_orchestra_backoffice.form.internal_link.node',
-                'siteId' => $siteId,
-                'attr' => array(
-                    'class' => 'orchestra-node-choice',
-                ),
-                'required' => true,
-            ));
-            $form->add('aliasId', 'choice', array(
-                'label' => 'open_orchestra_backoffice.form.internal_link.site_alias',
-                'attr' => $this->attributes,
-                'choices' => $this->getChoices($siteId),
-                'required' => false,
-            ));
-            if (!array_key_exists($nodeId, $form->get('nodeId')->getConfig()->getOption('choices'))) {
-                $data['nodeId'] = '';
-            }
-            if (!array_key_exists($aliasId, $form->get('aliasId')->getConfig()->getOption('choices'))) {
-                $data['aliasId'] = '';
-            }
-            $event->setData($data);
-        }
-    }
-
-    /**
      * @param $siteId
      *
      * @return array
