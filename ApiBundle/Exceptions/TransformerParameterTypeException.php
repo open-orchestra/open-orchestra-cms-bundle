323cbab365e7013d0ccd0b8c2f3784e3b6ab0fa6
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-09-27 17:28:54 +0200

    update deprecated annotation (#1890)

--- ApiBundle/Exceptions/TransformerParameterTypeException.php
+++ ApiBundle/Exceptions/TransformerParameterTypeException.php
@@ -4,12 +4,12 @@ namespace OpenOrchestra\ApiBundle\Exceptions;
 
 use OpenOrchestra\BaseApi\Exceptions\ApiException;
 
-@trigger_error('The '.__NAMESPACE__.'\TransformerParameterTypeException class is deprecated since version 1.2.0 and will be removed in 1.3.0, use OpenOrchestra\BaseApi\Exceptions\TransformerParameterTypeException', E_USER_DEPRECATED);
+@trigger_error('The '.__NAMESPACE__.'\TransformerParameterTypeException class is deprecated since version 1.2.0 and will be removed in 2.0, use OpenOrchestra\BaseApi\Exceptions\TransformerParameterTypeException', E_USER_DEPRECATED);
 
 /**
  * Class TransformerParameterTypeException
  *
- * @deprecated will be removed in 1.3.0, use OpenOrchestra\BaseApi\Exceptions\TransformerParameterTypeException instead
+ * @deprecated will be removed in 2.0 use OpenOrchestra\BaseApi\Exceptions\TransformerParameterTypeException instead
  */
 class TransformerParameterTypeException extends ApiException
 {
