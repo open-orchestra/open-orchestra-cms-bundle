2db0afbaa8975153a757e6864b59137b90cbb445
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-10-05 12:32:59 +0200

    use PaginationRepositoryInterface in LogRepositoryInterface

--- LogBundle/Repository/LogRepositoryInterface.php
+++ LogBundle/Repository/LogRepositoryInterface.php
@@ -1,34 +1,13 @@
 <?php
 
 namespace OpenOrchestra\LogBundle\Repository;
-use OpenOrchestra\Pagination\Configuration\FinderConfiguration;
-use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
+
+use OpenOrchestra\Pagination\Configuration\PaginationRepositoryInterface;
 
 
 /**
  * Class LogRepositoryInterface
  */
-interface LogRepositoryInterface
+interface LogRepositoryInterface extends PaginationRepositoryInterface
 {
-    /**
-     * @param PaginateFinderConfiguration $configuration
-     *
-     * @return mixed
-     * @throws \Doctrine\ODM\MongoDB\MongoDBException
-     */
-    public function findForPaginate(PaginateFinderConfiguration $configuration);
-
-    /**
-     * @param FinderConfiguration $configuration
-     *
-     * @return int
-     */
-    public function count(FinderConfiguration $configuration);
-
-    /**
-     * @param FinderConfiguration $configuration
-     *
-     * @return mixed
-     */
-    public function countWithFilter(FinderConfiguration $configuration);
 }
