d1e37820b3725a357d2c08a0e968ddb7776267b0
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-11-02 10:12:12 +0100

    change subbscriber to service

--- Backoffice/Tests/Form/Type/ContentTypeTypeTest.php
+++ Backoffice/Tests/Form/Type/ContentTypeTypeTest.php
@@ -15,6 +15,8 @@ class ContentTypeTypeTest extends AbstractBaseTestCase
     protected $translator;
     protected $class = 'content_type';
     protected $translatedLabel = 'existing option';
+    protected $contentTypeTypeSubscriber;
+    protected $contentTypeStatusableSubscriber;
 
     /**
      * Set up the test
@@ -24,16 +26,16 @@ class ContentTypeTypeTest extends AbstractBaseTestCase
         $this->translator = Phake::mock('Symfony\Component\Translation\TranslatorInterface');
         Phake::when($this->translator)->trans(Phake::anyParameters())->thenReturn($this->translatedLabel);
         $contentTypeOrderFieldTransformer = Phake::mock('Symfony\Component\Form\DataTransformerInterface');
-        $contentRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\ContentRepositoryInterface');
-        $statusRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\StatusRepositoryInterface');
+        $this->contentTypeTypeSubscriber = Phake::mock('OpenOrchestra\Backoffice\EventSubscriber\ContentTypeTypeSubscriber');
+        $this->contentTypeStatusableSubscriber = Phake::mock('OpenOrchestra\Backoffice\EventSubscriber\ContentTypeStatusableSubscriber');
 
         $this->form = new ContentTypeType(
             $this->class,
             $this->translator,
             array(),
             $contentTypeOrderFieldTransformer,
-            $contentRepository,
-            $statusRepository
+            $this->contentTypeTypeSubscriber,
+            $this->contentTypeStatusableSubscriber
         );
     }
 
@@ -59,7 +61,9 @@ class ContentTypeTypeTest extends AbstractBaseTestCase
         $this->form->buildForm($builder, array());
 
         Phake::verify($builder, Phake::times(8))->add(Phake::anyParameters());
-        Phake::verify($builder, Phake::times(2))->addEventSubscriber(Phake::anyParameters());
+        Phake::verify($builder)->addEventSubscriber($this->contentTypeTypeSubscriber);
+        Phake::verify($builder)->addEventSubscriber($this->contentTypeStatusableSubscriber);
+
         Phake::verify($this->translator)->trans('open_orchestra_backoffice.form.field_type.add');
         Phake::verify($this->translator)->trans('open_orchestra_backoffice.form.field_type.new');
         Phake::verify($this->translator)->trans('open_orchestra_backoffice.form.field_type.delete');
