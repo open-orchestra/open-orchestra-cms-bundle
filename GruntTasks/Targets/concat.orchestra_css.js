e90c1d9b6bf6dc809d12ec8404f70bff03d16105
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-11-21 13:31:58 +0100

    update theme css

--- GruntTasks/Targets/concat.orchestra_css.js
+++ GruntTasks/Targets/concat.orchestra_css.js
@@ -2,14 +2,8 @@ module.exports = {
   src: [
     'web/bundles/openorchestrabackoffice/font/Montserrat/css/fonts.css',
     'web/bundles/openorchestrabackoffice/font/Roboto/css/fonts.css',
-    'web/built/openorchestrabackoffice/css/theme/open-orchestra-v2.css',
-    'web/built/openorchestrabackoffice/css/theme/header/header.css',
-    'web/built/openorchestrabackoffice/css/theme/modal/modal.css',
-    'web/built/openorchestrabackoffice/css/theme/authentication/authentication.css',
-    'web/built/openorchestrabackoffice/css/theme/navigation/navigation.css',
-    'web/built/openorchestrabackoffice/css/theme/loader/ajaxLoader.css',
-    'web/built/openorchestrabackoffice/css/theme/languages/languages.css',
-    'web/built/openorchestrabackoffice/css/node/tree.css'
+    'web/built/openorchestrabackoffice/css/theme/open-orchestra.css',
+    'web/built/openorchestrabackoffice/css/theme/navigation/navigation.css'
   ],
   dest: 'web/built/orchestra.css'
 };
