b3bd7141af85399b93209a5eb3ae1ddb8e2ed15a
Nicolas ANNE <nicolas.anne@businessdecision.com>
2016-11-23 17:13:40 +0100

    add js

---
+++ BackofficeBundle/Resources/public/ecmascript/OpenOrchestra/Service/Form/Behavior/Loader.js
@@ -0,0 +1,46 @@
+/**
+ * @class Loader
+ */
+class Loader
+{
+    /**
+     * Constructor
+     */
+    constructor() {
+        this._behaviors = [];
+        Backbone.Events.on('form:activate', this._activate, this);
+        Backbone.Events.on('form:deactivate', this._deactivate, this);
+    }
+
+    /**
+     * @param {Object} $behavior
+     */
+    add($behavior) {
+        this._behaviors.push($behavior);
+    }
+
+    /**
+     * deactivate behavior
+     * 
+     * @param {Object} $form - jQuery element containing form
+     */
+    _activate($form) {
+        for (let behavior of this._behaviors) {
+            behavior.activate($(behavior.getSelector(), $form));
+        }
+    }
+
+    /**
+     * activate behavior
+     * 
+     * @param {Object} $form - jQuery element containing form
+     */
+    _deactivate($form) {
+        for (let behavior of this._behaviors) {
+            behavior.deactivate($(behavior.getSelector(), $form));
+        }
+    }
+}
+
+// unique instance of Loader
+export default (new Loader);
---
+++ BackofficeBundle/Resources/public/ecmascript/OpenOrchestra/Service/Form/Behavior/ScrollTable.js
@@ -0,0 +1,50 @@
+/**
+ * @class ScrollTable
+ */
+class ScrollTable
+{
+    /**
+     * activate behavior
+     * 
+     * @param {Object} $elements - jQuery elements matching selector
+     */
+    activate($elements) {
+        $elements.each(function() {
+            let $table = $(this);
+            let $fixedColumn = $table.clone().insertBefore($table).css({
+                position: 'absolute',
+                display: 'inline-block',
+                width: 'auto',
+                borderRight: '1px dotted #000000',
+                backgroudColor: '#000000',
+                zIndex: 2,
+            });
+            $table.css({zIndex: 1}).wrap($('<div>').css({overflow: 'auto'}));
+            $fixedColumn.find('th:not(:first-child),td:not(:first-child)').remove();
+            $fixedColumn.find('tr').each(function (i, elem) {
+                $(this).height($table.find('tr:eq(' + i + ')').height());
+            });
+        });
+    }
+
+    /**
+     * deactivate behavior
+     * 
+     * @param {Object} $elements - jQuery elements matching selector
+     */
+    deactivate($elements) {
+    }
+
+    /**
+     * return selector
+     * 
+     * @return {String}
+     */
+    getSelector() {
+        return 'table.scrollable';
+    }
+
+}
+
+// unique instance of ScrollTable
+export default (new ScrollTable);
