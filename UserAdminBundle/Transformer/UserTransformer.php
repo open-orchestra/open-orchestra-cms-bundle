58bf5a1f98971a4e2ae66e8917bf98c1651a9097
Amaury Lavieille <amaury.lavieille@gmail.com>
2016-12-07 14:15:20 +0100

    remove link user facade (#2018)

--- UserAdminBundle/Transformer/UserTransformer.php
+++ UserAdminBundle/Transformer/UserTransformer.php
@@ -4,18 +4,15 @@ namespace OpenOrchestra\UserAdminBundle\Transformer;
 
 use OpenOrchestra\BaseApi\Facade\FacadeInterface;
 use OpenOrchestra\BaseApi\Transformer\AbstractSecurityCheckerAwareTransformer;
+use OpenOrchestra\BaseApi\Transformer\AbstractTransformer;
 use OpenOrchestra\ModelInterface\Manager\MultiLanguagesChoiceManagerInterface;
 use OpenOrchestra\UserBundle\Document\User;
-use OpenOrchestra\UserAdminBundle\UserFacadeEvents;
-use OpenOrchestra\UserAdminBundle\Event\UserFacadeEvent;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
-use OpenOrchestra\Backoffice\Security\ContributionActionInterface;
 
 /**
  * Class UserTransformer
  */
-class UserTransformer extends AbstractSecurityCheckerAwareTransformer
+class UserTransformer extends AbstractTransformer
 {
     protected $eventDispatcher;
     protected $multiLanguagesChoiceManager;
@@ -24,15 +21,13 @@ class UserTransformer extends AbstractSecurityCheckerAwareTransformer
      * @param string                               $facadeClass
      * @param EventDispatcherInterface             $eventDispatcher
      * @param MultiLanguagesChoiceManagerInterface $multiLanguagesChoiceManager
-     * @param AuthorizationCheckerInterface        $authorizationChecker
      */
     public function __construct(
         $facadeClass,
         EventDispatcherInterface $eventDispatcher,
-        MultiLanguagesChoiceManagerInterface $multiLanguagesChoiceManager,
-        AuthorizationCheckerInterface $authorizationChecker
+        MultiLanguagesChoiceManagerInterface $multiLanguagesChoiceManager
     ) {
-        parent::__construct($facadeClass, $authorizationChecker);
+        parent::__construct($facadeClass);
         $this->eventDispatcher = $eventDispatcher;
         $this->multiLanguagesChoiceManager = $multiLanguagesChoiceManager;
     }
@@ -58,27 +53,6 @@ class UserTransformer extends AbstractSecurityCheckerAwareTransformer
             }
 
             $facade->groups = implode(',', $labels);
-
-            if ($this->authorizationChecker->isGranted(ContributionActionInterface::DELETE, $mixed)) {
-                $facade->addLink('_self_delete', $this->generateRoute(
-                    'open_orchestra_api_user_delete',
-                    array('userId' => $mixed->getId())
-                ));
-            }
-            if ($this->authorizationChecker->isGranted(ContributionActionInterface::EDIT, $mixed)) {
-                $facade->addLink('_self_form', $this->generateRoute(
-                    'open_orchestra_user_admin_user_form',
-                    array('userId' => $mixed->getId())
-                ));
-
-                $facade->addLink('_self_panel_password_change', $this->generateRoute(
-                    'open_orchestra_user_admin_user_change_password',
-                    array('userId' => $mixed->getId())));
-            }
-            $this->eventDispatcher->dispatch(
-                UserFacadeEvents::POST_USER_TRANSFORMATION,
-                new UserFacadeEvent($facade, $mixed)
-            );
         }
 
         return $facade;
