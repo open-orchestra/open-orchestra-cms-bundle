f0680d1ca13953b6daf5e0c7527a2393ce154a77
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-12-14 09:41:57 +0100

    update unit test

--- UserAdminBundle/Tests/Form/Type/RegistrationUserTypeTest.php
+++ UserAdminBundle/Tests/Form/Type/RegistrationUserTypeTest.php
@@ -14,7 +14,6 @@ class RegistrationUserTypeTest extends AbstractUserTypeTest
      * @var RegistrationUserType
      */
     protected $form;
-
     protected $class = 'OpenOrchestra\UserBundle\Document\User';
 
     /**
@@ -23,25 +22,51 @@ class RegistrationUserTypeTest extends AbstractUserTypeTest
     public function setUp()
     {
         parent::setUp();
-        $this->form = new RegistrationUserType($this->class);
-    }
+        $objectManager = Phake::mock('Doctrine\Common\Persistence\ObjectManager');
+        $user = Phake::mock('OpenOrchestra\UserBundle\Model\UserInterface');
+        Phake::when($user)->getGroups()->thenReturn(array());
+        $token = Phake::mock('Symfony\Component\Security\Core\Authentication\Token\TokenInterface');
+        Phake::when($token)->getUser()->thenReturn($user);
+        $tokenStorage = Phake::mock('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface');
+        Phake::when($tokenStorage)->getToken()->thenReturn($token);
+        $parameters = array(0 => 'en', 1 => 'fr');
 
-    /**
-     * Test name
-     */
-    public function testName()
-    {
-        $this->assertSame('oo_registration_user', $this->form->getName());
+        $this->form = new RegistrationUserType($objectManager, $tokenStorage, $this->class, $parameters);
     }
 
     /**
      * Test builder
+     *
+     * @param array   $options
+     * @param boolean $expectSubscriber
+     *
+     * @dataProvider provideOptions
      */
-    public function testBuilder()
+    public function testBuilder(array $options, $expectSubscriber, $nbrAdd)
     {
-        $this->form->buildForm($this->builder, array());
+        $this->form->buildForm($this->builder, $options);
 
-        Phake::verify($this->builder, Phake::times(5))->add(Phake::anyParameters());
+        Phake::verify($this->builder, Phake::times($nbrAdd))->add(Phake::anyParameters());
+
+        if ($expectSubscriber) {
+            Phake::verify($this->builder, Phake::times(2))->addEventSubscriber(Phake::anyParameters());
+        }
+    }
+
+    /**
+     * Provide form type options
+     *
+     * @return array
+     */
+    public function provideOptions()
+    {
+        $site = Phake::mock('OpenOrchestra\UserBundle\Model\UserInterface');
+        Phake::when($site)->getLanguageBySites()->thenReturn(array('en' => 'fakeLanguage', 'fr' => 'fakeLanguage'));
+
+        return array(
+            'without_groups_edition' => array(array('edit_groups' => 'false', 'required_password' => true, 'self_editing' => false, 'data' => $site,), true, 6),
+            'with_groups_edition' => array(array('edit_groups' => 'true',  'required_password' => true, 'self_editing' => true, 'data' => $site), false, 8)
+        );
     }
 
     /**
@@ -50,7 +75,53 @@ class RegistrationUserTypeTest extends AbstractUserTypeTest
     public function testResolver()
     {
         $this->form->configureOptions($this->resolver);
-
+        Phake::verify($this->resolver)->setDefaults(array(
+            'data_class' => $this->class,
+            'edit_groups' => true,
+            'self_editing' => false,
+            'group_enabled' => true,
+            'required_password' => false,
+            'group_render' => array(
+                'information' => array(
+                    'rank' => 0,
+                    'label' => 'open_orchestra_user_admin.form.user.group.information',
+                ),
+                'authentication' => array(
+                    'rank' => 1,
+                    'label' => 'open_orchestra_user_admin.form.user.group.authentication',
+                ),
+                'preference' => array(
+                    'rank' => 2,
+                    'label' => 'open_orchestra_user_admin.form.user.group.preference',
+                ),
+            ),
+            'sub_group_render' => array(
+                'contact_information' => array(
+                    'rank' => 0,
+                    'label' => 'open_orchestra_user_admin.form.user.sub_group.contact_information',
+                ),
+                'group' => array(
+                    'rank' => 1,
+                    'label' => 'open_orchestra_user_admin.form.user.sub_group.group',
+                ),
+                'profil' => array(
+                    'rank' => 2,
+                    'label' => 'open_orchestra_user_admin.form.user.sub_group.profil',
+                ),
+                'identifier' => array(
+                    'rank' => 0,
+                    'label' => 'open_orchestra_user_admin.form.user.sub_group.identifier',
+                ),
+                'backoffice' => array(
+                    'rank' => 0,
+                    'label' => 'open_orchestra_user_admin.form.user.sub_group.backoffice',
+                ),
+                'language' => array(
+                    'rank' => 1,
+                    'label' => 'open_orchestra_user_admin.form.user.sub_group.language',
+                ),
+            ),
+        ));
         Phake::verify($this->resolver)->setDefaults(Phake::anyParameters());
     }
 }
--- UserAdminBundle/Tests/Form/Type/UserTypeTest.php
+++ UserAdminBundle/Tests/Form/Type/UserTypeTest.php
@@ -16,7 +16,6 @@ class UserTypeTest extends AbstractUserTypeTest
     protected $form;
 
     protected $class = 'OpenOrchestra\UserBundle\Document\User';
-    protected $twig;
 
     /**
      * Set up the test
@@ -31,7 +30,6 @@ class UserTypeTest extends AbstractUserTypeTest
         Phake::when($token)->getUser()->thenReturn($user);
         $tokenStorage = Phake::mock('Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface');
         Phake::when($tokenStorage)->getToken()->thenReturn($token);
-        $this->twig = Phake::mock('Twig_Environment');
         $parameters = array(0 => 'en', 1 => 'fr');
 
         $this->form = new UserType($objectManager, $tokenStorage, $this->class, $parameters);
@@ -75,8 +73,8 @@ class UserTypeTest extends AbstractUserTypeTest
         Phake::when($site)->getLanguageBySites()->thenReturn(array('en' => 'fakeLanguage', 'fr' => 'fakeLanguage'));
 
         return array(
-            'without_groups_edition' => array(array('edit_groups' => 'false', 'self_editing' => false, 'data' => $site,), true, 5),
-            'with_groups_edition' => array(array('edit_groups' => 'true', 'self_editing' => true, 'data' => $site), false, 7)
+            'without_groups_edition' => array(array('edit_groups' => 'false', 'required_password' => false, 'self_editing' => false, 'data' => $site,), true, 5),
+            'with_groups_edition' => array(array('edit_groups' => 'true', 'required_password' => false, 'self_editing' => true, 'data' => $site), false, 7)
         );
     }
 
@@ -91,6 +89,7 @@ class UserTypeTest extends AbstractUserTypeTest
             'edit_groups' => true,
             'self_editing' => false,
             'group_enabled' => true,
+            'required_password' => false,
             'group_render' => array(
                 'information' => array(
                     'rank' => 0,
