0cd6da3f8d1400988c533a20aef97571c8e0a9ee
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2016-12-16 09:38:15 +0100

    fix test

--- UserAdminBundle/Tests/Form/Type/RegistrationUserTypeTest.php
+++ UserAdminBundle/Tests/Form/Type/RegistrationUserTypeTest.php
@@ -23,101 +23,30 @@ class RegistrationUserTypeTest extends AbstractUserTypeTest
     public function setUp()
     {
         parent::setUp();
-        $parameters = array(0 => 'en', 1 => 'fr');
-        $userProfilSubscriber = Phake::mock(EventSubscriberInterface::class);
-        $userGroupSubscriber = Phake::mock(EventSubscriberInterface::class);
 
-        $this->form = new RegistrationUserType($this->class, $parameters, $userProfilSubscriber, $userGroupSubscriber);
+        $this->form = new RegistrationUserType();
+    }
+
+    /**
+     * Test get parent
+     */
+    public function testGetParent()
+    {
+        $this->assertEquals($this->form->getParent(), 'oo_user');
     }
 
     /**
      * Test builder
-     *
-     * @param array   $options
-     * @param boolean $expectSubscriber
-     *
-     * @dataProvider provideOptions
      */
-    public function testBuilder(array $options, $expectSubscriber, $nbrAdd)
+    public function testBuilder()
     {
-        $this->form->buildForm($this->builder, $options);
+        $this->form->buildForm($this->builder, array());
 
-        Phake::verify($this->builder, Phake::times($nbrAdd))->add(Phake::anyParameters());
-
-        if ($expectSubscriber) {
-            Phake::verify($this->builder, Phake::times(2))->addEventSubscriber(Phake::anyParameters());
-        }
-    }
-
-    /**
-     * Provide form type options
-     *
-     * @return array
-     */
-    public function provideOptions()
-    {
-        $site = Phake::mock('OpenOrchestra\UserBundle\Model\UserInterface');
-        Phake::when($site)->getLanguageBySites()->thenReturn(array('en' => 'fakeLanguage', 'fr' => 'fakeLanguage'));
-
-        return array(
-            'without_groups_edition' => array(array('edit_groups' => 'false', 'required_password' => true, 'self_editing' => false, 'data' => $site,), true, 6),
-            'with_groups_edition' => array(array('edit_groups' => 'true',  'required_password' => true, 'self_editing' => true, 'data' => $site), false, 8)
-        );
-    }
-
-    /**
-     * Test configureOptions
-     */
-    public function testResolver()
-    {
-        $this->form->configureOptions($this->resolver);
-        Phake::verify($this->resolver)->setDefaults(array(
-            'data_class' => $this->class,
-            'edit_groups' => true,
-            'self_editing' => false,
-            'group_enabled' => true,
-            'required_password' => false,
-            'group_render' => array(
-                'information' => array(
-                    'rank' => 0,
-                    'label' => 'open_orchestra_user_admin.form.user.group.information',
-                ),
-                'authentication' => array(
-                    'rank' => 1,
-                    'label' => 'open_orchestra_user_admin.form.user.group.authentication',
-                ),
-                'preference' => array(
-                    'rank' => 2,
-                    'label' => 'open_orchestra_user_admin.form.user.group.preference',
-                ),
-            ),
-            'sub_group_render' => array(
-                'contact_information' => array(
-                    'rank' => 0,
-                    'label' => 'open_orchestra_user_admin.form.user.sub_group.contact_information',
-                ),
-                'group' => array(
-                    'rank' => 1,
-                    'label' => 'open_orchestra_user_admin.form.user.sub_group.group',
-                ),
-                'profil' => array(
-                    'rank' => 2,
-                    'label' => 'open_orchestra_user_admin.form.user.sub_group.profil',
-                ),
-                'identifier' => array(
-                    'rank' => 0,
-                    'label' => 'open_orchestra_user_admin.form.user.sub_group.identifier',
-                ),
-                'backoffice' => array(
-                    'rank' => 0,
-                    'label' => 'open_orchestra_user_admin.form.user.sub_group.backoffice',
-                ),
-                'language' => array(
-                    'rank' => 1,
-                    'label' => 'open_orchestra_user_admin.form.user.sub_group.language',
-                ),
-            ),
+        Phake::verify($this->builder)->add('username', 'text', array(
+            'label' => 'form.username',
+            'translation_domain' => 'FOSUserBundle',
+            'group_id' => 'information',
+            'sub_group_id' => 'contact_information',
         ));
-        Phake::verify($this->resolver)->setDefaults(Phake::anyParameters());
     }
 }
