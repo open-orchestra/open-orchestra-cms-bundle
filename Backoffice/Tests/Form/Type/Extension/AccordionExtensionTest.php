467b3cfcc78950154d2607401c9371244057f36f
Nicolas ANNE <nicolas.anne@businessdecision.com>
2017-01-06 10:00:46 +0100

    refresh test

--- Backoffice/Tests/Form/Type/Extension/AccordionExtensionTest.php
+++ Backoffice/Tests/Form/Type/Extension/AccordionExtensionTest.php
@@ -36,7 +36,6 @@ class AccordionExtensionTest extends AbstractBaseTestCase
         Phake::when($formConfig)->getType()->thenReturn($resolvedFormType);
         Phake::when($formConfig)->getOption('columns')->thenReturn(array('fakeColumn0', 'fakeColumn1', 'fakeNonExistingColumn'));
 
-
         $form = Phake::mock('Symfony\Component\Form\FormInterface');
         Phake::when($form)->getConfig()->thenReturn($formConfig);
         Phake::when($form)->getParent()->thenReturn($form);
@@ -44,11 +43,30 @@ class AccordionExtensionTest extends AbstractBaseTestCase
         Phake::when($form)->has('fakeColumn1')->thenReturn(true);
         Phake::when($form)->has('fakeNonExistingColumn')->thenReturn(false);
 
+        $formColumn0Config = Phake::mock('Symfony\Component\Form\FormConfigInterface');
+        Phake::when($formColumn0Config)->getOption('label')->thenReturn('fakeColumn0Label');
+
+        $formColumn0 = Phake::mock('Symfony\Component\Form\FormInterface');
+        Phake::when($form)->get('fakeColumn0')->thenReturn($formColumn0);
+        Phake::when($formColumn0)->getConfig()->thenReturn($formColumn0Config);
+        Phake::when($formColumn0)->getData()->thenReturn('fakeColumn0Data');
+
+        $formColumn1Config = Phake::mock('Symfony\Component\Form\FormConfigInterface');
+        Phake::when($formColumn1Config)->getOption('label')->thenReturn('fakeColumn1Label');
+
+        $formColumn1 = Phake::mock('Symfony\Component\Form\FormInterface');
+        Phake::when($form)->get('fakeColumn1')->thenReturn($formColumn1);
+        Phake::when($formColumn1)->getConfig()->thenReturn($formColumn1Config);
+        Phake::when($formColumn1)->getData()->thenReturn('fakeColumn1Data');
+
         $formView = Phake::mock('Symfony\Component\Form\FormView');
 
         $this->formExtension->buildView($formView, $form, array());
 
-        $this->assertEquals($formView->vars['attr']['class'], 'collection-sortable');
+        $this->assertEquals($formView->vars['columns'], array(
+            'fakeColumn0Label' => 'fakeColumn0Data',
+            'fakeColumn1Label' => 'fakeColumn1Data',
+        ));
     }
 
     /**
