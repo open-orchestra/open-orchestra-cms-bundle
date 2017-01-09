903117bc86ab6eac3af08112dd13d8f78f56ebc2
itkg-ngilain <noel.gilain@businessdecision.com>
2017-01-09 15:35:42 +0100

    Working version of the workflow parameter form

--- WorkflowAdminBundle/Controller/Admin/ParametersController.php
+++ WorkflowAdminBundle/Controller/Admin/ParametersController.php
@@ -29,14 +29,19 @@ class ParametersController extends AbstractAdminController
         $this->denyAccessUnlessGranted(ContributionActionInterface::EDIT, StatusInterface::ENTITY_TYPE);
 
         $statusCollection = $this->get('open_orchestra_model.repository.status')->findNotOutOfWorkflow();
-            $data = array(
+        $data = array(
             'statuses' => $statusCollection,
             'labels'   => array(
-                $this->get('translator')->trans('open_orchestra_workflow_admin.status.initial_state'          , array(), 'interface'),
-                $this->get('translator')->trans('open_orchestra_workflow_admin.status.published_state'        , array(), 'interface'),
-                $this->get('translator')->trans('open_orchestra_workflow_admin.status.auto_publish_from_state', array(), 'interface'),
-                $this->get('translator')->trans('open_orchestra_workflow_admin.status.auto_unpublish_to_state', array(), 'interface'),
-                $this->get('translator')->trans('open_orchestra_workflow_admin.status.translation_state'      , array(), 'interface')
+                $this->get('translator')
+                    ->trans('open_orchestra_workflow_admin.status.initial_state'          , array(), 'interface'),
+                $this->get('translator')
+                    ->trans('open_orchestra_workflow_admin.status.published_state'        , array(), 'interface'),
+                $this->get('translator')
+                    ->trans('open_orchestra_workflow_admin.status.auto_publish_from_state', array(), 'interface'),
+                $this->get('translator')
+                    ->trans('open_orchestra_workflow_admin.status.auto_unpublish_to_state', array(), 'interface'),
+                $this->get('translator')
+                    ->trans('open_orchestra_workflow_admin.status.translation_state'      , array(), 'interface')
             )
         );
 
@@ -44,12 +49,93 @@ class ParametersController extends AbstractAdminController
             'action' => $this->generateUrl('open_orchestra_workflow_admin_parameters_form')
         ));
 
+        $updatedStatusIndexes = array();
+        if ($request->isMethod('POST')) {
+            $updatedStatusIndexes = $this->getStatusIndexesToUpdate(
+                $statusCollection,
+                $request->get('oo_workflow_parameters')
+            );
+        }
+
         $form->handleRequest($request);
-        $message = $this->get('translator')->trans('open_orchestra_backoffice.form.parameters.success');
+        $message = $this->get('translator')->trans('open_orchestra_workflow_admin.form.parameters.success');
         if ($this->handleForm($form, $message)) {
-            $this->dispatchEvent(StatusEvents::STATUS_UPDATE, new StatusEvent($status));
+            foreach ($updatedStatusIndexes as $statusIndex) {
+                $this->dispatchEvent(
+                    StatusEvents::STATUS_UPDATE,
+                    new StatusEvent($statusCollection[$statusIndex])
+                );
+            }
         }
 
         return $this->renderAdminForm($form);
+    }
+
+    /**
+     * Get the list of status to update in $statusCollection
+     * Only indexes in $statusCollection are returned
+     *
+     * @param array $statusCollection
+     * @param array $formData
+     *
+     * @return array
+     */
+    protected function getStatusIndexesToUpdate(array $statusCollection, array $formData)
+    {
+        $statusMap = array();
+        $updatedStatusIndexes = array();
+        $formStatuses = $formData['statuses'];
+
+        foreach ($statusCollection as $index => $status) {
+            $statusMap[$index] = array(
+                'initialState'         => $status->isInitialState(),
+                'publishedState'       => $status->isPublishedState(),
+                'autoPublishFromState' => $status->isAutoPublishFromState(),
+                'autoUnpublishToState' => $status->isAutoUnpublishToState(),
+                'translationState'     => $status->isTranslationState()
+            );
+        }
+
+        foreach ($statusMap as $statusIndex => $oldParameters) {
+            foreach ($oldParameters as $parameter => $oldValue) {
+                if ($this->isParameterTurnedOn($parameter, $oldValue, $formStatuses, $statusIndex)
+                    || $this->isParameterTurnedOff($parameter, $oldValue, $formStatuses, $statusIndex)
+                ) {
+                    $updatedStatusIndexes[] = $statusIndex;
+                }
+            }
+        }
+
+        return $updatedStatusIndexes;
+    }
+
+    /**
+     * Check if $parameter is requested to be turned on
+     *
+     * @param string  $parameter
+     * @param boolean $oldValue
+     * @param array   $formStatuses
+     * @param string  $statusIndex
+     *
+     * @return boolean
+     */
+    protected function isParameterTurnedOn($parameter, $oldValue, array $formStatuses, $statusIndex)
+    {
+        return (!$oldValue && isset($formStatuses[$statusIndex]) && isset($formStatuses[$statusIndex][$parameter]));
+    }
+
+    /**
+     * Check if $parameter is requested to be turned on
+     *
+     * @param string  $parameter
+     * @param boolean $oldValue
+     * @param array   $formStatuses
+     * @param string  $statusIndex
+     *
+     * @return boolean
+     */
+    protected function isParameterTurnedOff($parameter, $oldValue, array $formStatuses, $statusIndex)
+    {
+        return ($oldValue && (!isset($formStatuses[$statusIndex]) || !isset($formStatuses[$statusIndex][$parameter])));
     }
 }
