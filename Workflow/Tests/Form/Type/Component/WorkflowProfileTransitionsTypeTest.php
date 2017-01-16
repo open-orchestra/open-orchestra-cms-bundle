a32d2c9d08cfcc073f7e68c4a8af59f24ba74a44
itkg-ngilain <noel.gilain@businessdecision.com>
2017-01-16 12:40:08 +0100

    Add WorkflowProfileTransitionsTypeTest

---
+++ Workflow/Tests/Form/Type/Component/WorkflowProfileTransitionsTypeTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace OpenOrchestra\Workflow\Tests\Form\Type\Component;
+
+use OpenOrchestra\BaseBundle\Tests\AbstractTest\AbstractBaseTestCase;
+use Phake;
+use OpenOrchestra\Workflow\Form\Type\Component\WorkflowProfileTransitionsType;
+
+/**
+ * Class WorkflowProfileTransitionsTypeTest
+ */
+class WorkflowProfileTransitionsTypeTest extends AbstractBaseTestCase
+{
+    protected $form;
+    protected $dataClass = 'data-class';
+
+    /**
+     * Set up the test
+     */
+    public function setUp()
+    {
+        $this->form = new WorkflowProfileTransitionsType($this->dataClass);
+    }
+
+    /**
+     * Test instance
+     */
+    public function testInstance()
+    {
+        $this->assertInstanceOf('Symfony\Component\Form\AbstractType', $this->form);
+    }
+
+    /**
+     * Test getName
+     */
+    public function testGetName()
+    {
+        $this->assertSame('oo_workflow_profile_transitions', $this->form->getName());
+    }
+
+    public function testBuildForm()
+    {
+        $builder = Phake::mock('Symfony\Component\Form\FormBuilderInterface');
+        $options = array('statuses' => array($this->generateStatus('1'), $this->generateStatus('2')));
+        $expectedChoices = array(
+            '1-1' => '1-1',
+            '1-2' => '1-2',
+            '2-1' => '2-1',
+            '2-2' => '2-2'
+        );
+
+        $this->form->buildForm($builder, $options);
+
+        Phake::verify($builder)->add('transitions', 'oo_workflow_transitions_collection', array(
+            'required' => false,
+            'choices'  => $expectedChoices,
+            'statuses' => $options['statuses']
+        ));
+    }
+
+    /**
+     * Test configureOptions
+     */
+    public function testConfigureOptions()
+    {
+        $resolver = Phake::mock('Symfony\Component\OptionsResolver\OptionsResolver');
+
+        $this->form->configureOptions($resolver);
+
+        Phake::verify($resolver)->setDefaults(array(
+            'data_class' => $this->dataClass,
+            'statuses'   => array()
+        ));
+    }
+
+    /**
+     * Generate a Phake status
+     *
+     * @param string $statusId
+     *
+     * @return Phake_IMock
+     */
+    protected function generateStatus($statusId)
+    {
+        $status = Phake::mock('OpenOrchestra\ModelInterface\Model\StatusInterface');
+        Phake::when($status)->getId()->thenReturn($statusId);
+        Phake::when($status)->getLabel(Phake::anyParameters())->thenReturn('label-' . $statusId);
+
+        return $status;
+    }
+}
