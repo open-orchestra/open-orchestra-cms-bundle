d3a07fe1acec252202aa54ba7f7fb7af84133348
Nicolas ANNE <nicolas.anne@businessdecision.com>
2017-02-13 10:09:01 +0100

    add js date formatter

--- BackofficeBundle/Resources/public/ecmascript/OpenOrchestra/Service/Content/CellFormatter/DateFormatter.js
+++ BackofficeBundle/Resources/public/ecmascript/OpenOrchestra/Service/Content/CellFormatter/DateFormatter.js
@@ -22,9 +22,36 @@ class DateFormatter extends AbstractCellFormatter
      */
     format(field) {
         return function(td, cellData, rowData) {
-            let dateFormat = $.datepicker.regional[Application.getContext().language].dateFormat;
-            let dateDetails = cellData.split(' ');
-            $(td).html($.datepicker.formatDate(dateDetails[0], cellData) + ' ' + dateDetails[1]);
+
+            let getGeneralPattern = function (originPattern) {
+                return new RegExp(originPattern
+                .replace('d', '[0-9]{2}')
+                .replace('Y', '[0-9]{4}')
+                .replace('m', '[0-9]{2}')
+                .replace('H', '[0-9]{2}')
+                .replace('i', '[0-9]{2}')
+                .replace('s', '[0-9]{2}')
+                .replace('O', '.*')
+                .replace(/\:/g, '\\:'));
+            };
+            
+            let destinationDateFormat = $.datepicker.regional[Application.getContext().language].dateFormat + ' H:i:s';
+            let originDateFormat = Application.getContext().dateFormat
+            
+            let dd = getGeneralPattern(originDateFormat.replace('d', '([0-9]{2})'));
+            let mm = getGeneralPattern(originDateFormat.replace('m', '([0-9]{2})'));
+            let yy = getGeneralPattern(originDateFormat.replace('Y', '([0-9]{4})'));
+            let H = getGeneralPattern(originDateFormat.replace('H', '([0-9]{2})'));
+            let i = getGeneralPattern(originDateFormat.replace('i', '([0-9]{2})'));
+            let s = getGeneralPattern(originDateFormat.replace('s', '([0-9]{2})'));
+            
+            $(td).html(destinationDateFormat
+                .replace('dd', dd.exec(cellData)[1])
+                .replace('mm', mm.exec(cellData)[1])
+                .replace('yy', yy.exec(cellData)[1])
+                .replace('H', H.exec(cellData)[1])
+                .replace('i', i.exec(cellData)[1])
+                .replace('s', s.exec(cellData)[1]));
         }
     }
 }
