5a4490d123f78c31fcf0366fe12cb4a98508bcff
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-02-22 15:21:47 +0100

    remove activate in content type form view

--- BackofficeBundle/Resources/public/ecmascript/OpenOrchestra/Application/View/ContentType/ContentTypeFormView.js
+++ BackofficeBundle/Resources/public/ecmascript/OpenOrchestra/Application/View/ContentType/ContentTypeFormView.js
@@ -40,9 +40,6 @@ class ContentTypeFormView extends mix(AbstractFormView).with(FormViewButtonsMixi
         this._$formRegion = $('.form-edit', this.$el);
         super.render();
         this._toggleAlwaysShared();
-        $('.nav-tabs a.nav-tab-preference', this._$formRegion).tab('show');
-        $('.tab-content .tab-preference', this._$formRegion).addClass('active');
-
 
         return this;
     }
