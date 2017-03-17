637d468a48bedb2416c3f91f087c3c79ca3f48aa
Amaury Lavieille <amaury.lavieille@gmail.com>
2017-03-17 09:44:01 +0100

    remove useless code (#2165)
    * remove useless code
    
    * fix translations

--- Backoffice/Repository/GroupRepositoryInterface.php
+++ Backoffice/Repository/GroupRepositoryInterface.php
@@ -10,22 +10,6 @@ use OpenOrchestra\Pagination\Configuration\PaginateFinderConfiguration;
  */
 interface GroupRepositoryInterface
 {
-
-    /**
-     * Find all groups linked to a site
-     *
-     * @return array
-     */
-    public function findAllWithSite();
-
-    /**
-     * Find all groups linked to site with $id
-     *
-     * @param string $id   The site id
-     * @return array
-     */
-    public function findAllWithSiteId($id);
-
     /**
      * @param PaginateFinderConfiguration $configuration
      * @param array                       $siteIds
