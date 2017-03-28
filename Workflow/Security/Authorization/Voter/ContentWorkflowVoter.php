b2447c7a08f9247e1e0f3f02d7f02f9f3d6f12aa
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-03-28 14:24:27 +0200

    fix worklow voter content

--- Workflow/Security/Authorization/Voter/ContentWorkflowVoter.php
+++ Workflow/Security/Authorization/Voter/ContentWorkflowVoter.php
@@ -3,6 +3,8 @@
 namespace OpenOrchestra\Workflow\Security\Authorization\Voter;
 
 use OpenOrchestra\ModelInterface\Model\ContentInterface;
+use OpenOrchestra\ModelInterface\Model\ContentTypeInterface;
+use OpenOrchestra\ModelInterface\Model\StatusableInterface;
 use OpenOrchestra\UserBundle\Model\UserInterface;
 
 /**
@@ -25,9 +27,21 @@ class ContentWorkflowVoter extends AbstractWorkflowVoter
      *
      * @param StatusableInterface $subject
      * @param UserInterface       $user
+     *
+     * @return boolean
      */
     protected function isInPerimeter($subject, UserInterface $user)
     {
-        return $this->isSubjectInPerimeter($subject->getContentType(), $user, ContentInterface::ENTITY_TYPE);
+        return $this->isSubjectInPerimeter($subject->getContentType(), $user, ContentTypeInterface::ENTITY_TYPE);
+    }
+
+    /**
+     * @param StatusableInterface $subject
+     *
+     * @return string
+     */
+    protected function getSubjectEntityType($subject)
+    {
+        return $subject->getContentType();
     }
 }
