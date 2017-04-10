f7e2dc4390690fa0f7af8cdff2f449796712a8c9
itkg-nanne <nicolas.anne@businessdecision.com>
2017-04-10 14:49:44 +0200

    remove break

--- ApiBundle/Controller/NodeController.php
+++ ApiBundle/Controller/NodeController.php
@@ -94,8 +94,6 @@ class NodeController extends BaseController
         $this->denyAccessUnlessGranted(ContributionActionInterface::DELETE, $node);
 
         if (!$this->get('open_orchestra_backoffice.business_rules_manager')->isGranted(ContributionActionInterface::DELETE, $node)) {
-            var_dump('throw');
-            die();
             throw new NodeNotDeletableException();
         }
 
