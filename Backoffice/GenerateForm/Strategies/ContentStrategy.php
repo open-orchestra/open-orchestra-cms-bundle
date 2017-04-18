a5e658ea1fa56a157b47307256fd88238f078393
Amaury Lavieille <amaury.lavieille@businessdecision.com>
2017-04-18 16:51:37 +0200

    remove parameter configurable content on content blocks'

--- Backoffice/GenerateForm/Strategies/ContentStrategy.php
+++ Backoffice/GenerateForm/Strategies/ContentStrategy.php
@@ -2,10 +2,8 @@
 
 namespace OpenOrchestra\Backoffice\GenerateForm\Strategies;
 
-use OpenOrchestra\Backoffice\Validator\Constraints\ContentTemplate;
 use OpenOrchestra\DisplayBundle\DisplayBlock\Strategies\ContentStrategy as BaseContentStrategy;
 use OpenOrchestra\ModelInterface\Model\BlockInterface;
-use Symfony\Component\Form\FormBuilderInterface;
 
 /**
  * Class ContentStrategy
@@ -20,28 +18,6 @@ class ContentStrategy extends AbstractBlockStrategy
     public function support(BlockInterface $block)
     {
         return BaseContentStrategy::NAME === $block->getComponent();
-    }
-
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array                $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('contentTemplateEnabled', 'checkbox', array(
-            'label' => 'open_orchestra_backoffice.form.content_list.content_template_enabled.title',
-            'attr' => array('help_text' => 'open_orchestra_backoffice.form.content_list.content_template_enabled.helper'),
-            'required' => false,
-            'group_id' => 'data',
-            'sub_group_id' => 'content',
-        ));
-        $builder->add('contentTemplate', 'oo_tinymce', array(
-            'required' => false,
-            'label' => 'open_orchestra_backoffice.form.content_list.content_template',
-            'constraints' => new ContentTemplate(),
-            'group_id' => 'data',
-            'sub_group_id' => 'content',
-        ));
     }
 
     /**
