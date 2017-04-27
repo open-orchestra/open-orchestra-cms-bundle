2eddfb2888967bba0e3f01fd6861c9fd29dd0e6d
Noël GILAIN <noel.gilain@businessdecision.com>
2017-04-27 17:42:31 +0200

    2 0 remove deleted node from perimeters (#2277)
    * Remove deleted nodes from perimeters
    
    * Update unit tests
    
    * Update nodeEvents::NODE_REMOVED to nodeEvents::NODE_HARD_DELETED

--- GroupBundle/Tests/EventSubscriber/GroupPerimeterSubscriberTest.php
+++ GroupBundle/Tests/EventSubscriber/GroupPerimeterSubscriberTest.php
@@ -6,6 +6,8 @@ use OpenOrchestra\BaseBundle\Tests\AbstractTest\AbstractBaseTestCase;
 use OpenOrchestra\GroupBundle\EventSubscriber\GroupPerimeterSubscriber;
 use Phake;
 use Symfony\Component\Form\FormEvents;
+use OpenOrchestra\ModelInterface\Model\NodeInterface;
+use OpenOrchestra\ModelInterface\NodeEvents;
 
 /**
  * Class GroupPerimeterSubscriberTest
@@ -16,7 +18,12 @@ class GroupPerimeterSubscriberTest extends AbstractBaseTestCase
      * @var GroupPerimeterSubscriber
      */
     protected $subscriber;
+    protected $groupRepository;
     protected $event;
+    protected $nodeEvent;
+    protected $nodePreviousPath = 'previousPath';
+    protected $nodePath = 'nodePath';
+    protected $siteId = 'siteId';
 
     /**
      * Set up the test
@@ -25,10 +32,21 @@ class GroupPerimeterSubscriberTest extends AbstractBaseTestCase
     {
         $this->event = Phake::mock('Symfony\Component\Form\FormEvent');
 
-        $groupRepository = Phake::mock('OpenOrchestra\Backoffice\Repository\GroupRepositoryInterface');
-        $siteRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\SiteRepositoryInterface');
+        $this->groupRepository = Phake::mock('OpenOrchestra\Backoffice\Repository\GroupRepositoryInterface');
 
-        $this->subscriber = new GroupPerimeterSubscriber($groupRepository, $siteRepository);
+        $site = Phake::mock('OpenOrchestra\ModelInterface\Model\SiteInterface');
+        Phake::when($site)->getId()->thenReturn($this->siteId);
+        $siteRepository = Phake::mock('OpenOrchestra\ModelInterface\Repository\SiteRepositoryInterface');
+        Phake::when($siteRepository)->findOneBySiteId(Phake::anyParameters())->thenReturn($site);
+
+        $node = Phake::mock('OpenOrchestra\ModelInterface\Model\NodeInterface');
+        Phake::when($node)->getPath()->thenReturn($this->nodePath);
+
+        $this->nodeEvent = Phake::mock('OpenOrchestra\ModelInterface\Event\NodeEvent');
+        Phake::when($this->nodeEvent)->getNode()->thenReturn($node);
+        Phake::when($this->nodeEvent)->getPreviousPath()->thenReturn($this->nodePreviousPath);
+
+        $this->subscriber = new GroupPerimeterSubscriber($this->groupRepository, $siteRepository);
     }
 
     /**
@@ -45,6 +63,8 @@ class GroupPerimeterSubscriberTest extends AbstractBaseTestCase
     public function testEventSubscribed()
     {
         $this->assertArrayHasKey(FormEvents::POST_SUBMIT, $this->subscriber->getSubscribedEvents());
+        $this->assertArrayHasKey(NodeEvents::PATH_UPDATED, $this->subscriber->getSubscribedEvents());
+        $this->assertArrayHasKey(NodeEvents::NODE_HARD_DELETED, $this->subscriber->getSubscribedEvents());
     }
 
     /**
@@ -65,5 +85,28 @@ class GroupPerimeterSubscriberTest extends AbstractBaseTestCase
         $this->subscriber->postSubmit($this->event);
 
         Phake::verify($group)->addPerimeter(Phake::anyParameters());
+    }
+
+    public function testUpdateNodeInPerimeter()
+    {
+        $this->subscriber->updateNodeInPerimeter($this->nodeEvent);
+
+        Phake::verify($this->groupRepository)->updatePerimeterItem(
+            NodeInterface::ENTITY_TYPE,
+            $this->nodePreviousPath,
+            $this->nodePath,
+            $this->siteId
+        );
+    }
+
+    public function testRemoveNodeFromPerimeter()
+    {
+        $this->subscriber->removeNodeFromPerimeter($this->nodeEvent);
+
+        Phake::verify($this->groupRepository)->removePerimeterItem(
+            NodeInterface::ENTITY_TYPE,
+            $this->nodePath,
+            $this->siteId
+        );
     }
 }
